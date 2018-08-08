551f306d0b0c6d55277514022c12f7e7d66e0c1c
EXT Olivera <oliveraa@molinosagro.com.ar>
2018-08-08 14:56:23 -0300

    Commit: 2018-08-08 14:56:22Z

--- Base de Datos/Security/dataagrot_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [dataagrot]
-    WITH PASSWORD = N'cpk,qloePmzafbc!$lcu{y5fmsFT7_&#$!~<9sq|!mkzf6k$', SID = 0x8EA40A13C372304C9876D6A2BC87EAF0, DEFAULT_DATABASE = [DataAgro2], DEFAULT_LANGUAGE = [us_english];
-
