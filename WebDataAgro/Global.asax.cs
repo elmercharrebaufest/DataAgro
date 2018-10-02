b95e1b589d13ee44700587509f5ae4bef264ef43
EXT Olivera <oliveraa@Molinosagro.com.ar>
2018-10-02 17:55:13 -0300

    Commit: 2018-10-02 17:55:13Z

--- WebDataAgro/Global.asax.cs
+++ WebDataAgro/Global.asax.cs
@@ -66,6 +66,7 @@ namespace WebDataAgro
         protected void Application_Error(object sender, EventArgs e)
         {
             Exception exception = Server.GetLastError();
+            DependencyResolver.Current.GetService<ILogger>().Error("Application_Error: ", exception);
             Response.Clear();
 
             HttpException httpException = exception as HttpException;
