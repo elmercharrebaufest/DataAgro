ba49aa35c09a5a2cba37921bbfe7370854e64e1b
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-01-25 16:02:44 -0300

    Commit: 2019-01-25 16:02:43Z

---
+++ Molinos.DataAgro.Test/Controllers/ReporteCompraNetControllerTest.cs
@@ -0,0 +1,90 @@
+﻿using Molinos.DataAgro.Entities.Dto;
+using Molinos.DataAgro.Entities.Entities;
+using Molinos.DataAgro.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebDataAgro.Controllers;
+using WebDataAgro.Models;
+using static WebDataAgro.MvcApplication;
+
+namespace Molinos.DataAgro.Test.Controllers
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable", Justification = "Test")]
+    [TestFixture]
+    public class ReporteCompraNetControllerTest
+    {
+        private ReporteCompraNetController target;
+        private Mock<IReportesManager> reportesManagerMock;
+        private JavaScriptSerializer serializer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.serializer = new JavaScriptSerializer();
+            reportesManagerMock = new Mock<IReportesManager>();
+            HttpContext.Current = Mock.FakeContext.FakeHttpContext();
+            HttpContext.Current.Session["perfil"] = 1;
+            target = new ReporteCompraNetController(reportesManagerMock.Object);
+        }
+
+        [Test]
+        public void IndexOk()
+        {
+            var result = target.Index() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.That(result.ViewName, Is.Null.Or.Empty);
+        }
+
+        [Test]
+        public void PartialReporteCompraNetTest()
+        {
+            var fecha = new DateTime(2018,10,26);
+            reportesManagerMock.Setup(x => x.TraerTodosHedgeMaterial(fecha, fecha)).Returns(new List<HedgeMaterialDto>());
+            reportesManagerMock.Setup(x => x.TraerAgenteDeCompra(fecha, fecha)).Returns(new List<AgenteCompraDto>());
+            var resultado = target.PartialReporteCompraNet("26-10-2018", "26-10-2018") as PartialViewResult;
+
+            Assert.NotNull(resultado);
+        }
+
+        [Test]
+        public void DetalleExcelTest()
+        {
+            var fecha = new DateTime(2018, 10, 26);
+            var reportes = new ParamReportes() { ComercialActual = 1 };
+            reportesManagerMock.Setup(x => x.DetallePosicion(1, 10, 2018, fecha, fecha, null)).Returns(new ExcelDetallePosicionDto()
+            {
+                Headers = new string[] { "1", "2" },
+                Data = new List<string[]>() { new string[] { "a", "b" } },
+                Name = "B",
+                SheetName = "C"
+            });
+
+            var result = target.DetalleExcel(10, 2018, 1, "26-10-2018", "26-10-2018", null);
+            Assert.NotNull(result);
+        }
+        [Test]
+        public void ReporteComprasDelDiaTest()
+        {
+            var fecha = new DateTime(2018, 10, 26);
+            reportesManagerMock.Setup(x => x.TraerAgenteDeCompra(fecha, fecha)).Returns(new List<AgenteCompraDto>());
+            reportesManagerMock.Setup(x => x.TraerToneladasGranoTipo(fecha, fecha)).Returns(new List<ToneladasGranoTipoDto>());
+            reportesManagerMock.Setup(x => x.TraerToneladasSojaSust(fecha, fecha)).Returns(new ReporteSojaSustDto());
+            reportesManagerMock.Setup(x => x.TraerPosicionCompras(fecha, fecha)).Returns(new List<PosicionComprasDto>());
+            reportesManagerMock.Setup(x => x.TraerMonedaCantidad(fecha, fecha)).Returns(new List<PrecioCantidadDto>());
+            reportesManagerMock.Setup(x => x.TraerTodosHedgeMaterial(fecha, fecha)).Returns(new List<HedgeMaterialDto>());
+            reportesManagerMock.Setup(x => x.TraerHedgeObjetivo(fecha, fecha)).Returns(new HedgeCargaObjetivoDto());
+            reportesManagerMock.Setup(x => x.TraerTcPromedio(fecha, fecha)).Returns(new HedgeTCPromedioDto());
+            var reportes = new ParamReportes() { ComercialActual = 1 };
+            reportesManagerMock.Setup(x => x.PosicionPorMaterial(fecha, fecha)).Returns(new List<ExcelPosicionMaterialDto>());
+
+            var result = target.ReporteComprasDelDia("26-10-2018", "26-10-2018");
+            Assert.NotNull(result);
+        }
+    }
+}
--- Molinos.DataAgro.Test/Controllers/ReportesControllerTest.cs
+++ Molinos.DataAgro.Test/Controllers/ReportesControllerTest.cs
@@ -3,7 +3,6 @@ using Molinos.DataAgro.Entities.Entities;
 using Molinos.DataAgro.Interfaces;
 using Moq;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
@@ -16,9 +15,11 @@ namespace Molinos.DataAgro.Test.Controllers
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable", Justification = "Test")]
     [TestFixture]
-    public class ReporteCompraNetControllerTest
+    public class ReportesControllerTest
     {
-        private ReporteCompraNetController target;
+        private ReportesController target;
+        private Mock<IComercialManager> comercialManagerMock;
+        private Mock<IHomeManager> homeManagerMock;
         private Mock<IReportesManager> reportesManagerMock;
         private JavaScriptSerializer serializer;
 
@@ -26,15 +27,19 @@ namespace Molinos.DataAgro.Test.Controllers
         public void SetUp()
         {
             this.serializer = new JavaScriptSerializer();
+            comercialManagerMock = new Mock<IComercialManager>();
+            homeManagerMock = new Mock<IHomeManager>();
             reportesManagerMock = new Mock<IReportesManager>();
             HttpContext.Current = Mock.FakeContext.FakeHttpContext();
+
             HttpContext.Current.Session["perfil"] = 1;
-            target = new ReporteCompraNetController(reportesManagerMock.Object);
+            target = new ReportesController(reportesManagerMock.Object, homeManagerMock.Object, comercialManagerMock.Object);
         }
 
         [Test]
         public void IndexOk()
         {
+            comercialManagerMock.Setup(x => x.ComercialExiste(GlobalVariables.IdActiveDirectory)).Returns(true);
             var result = target.Index() as ViewResult;
 
             Assert.NotNull(result);
@@ -42,49 +47,194 @@ namespace Molinos.DataAgro.Test.Controllers
         }
 
         [Test]
-        public void PartialReporteCompraNetTest()
+        public void TraerDatosComboReportesTest()
         {
-            var fecha = new DateTime(2018,08,26);
-            reportesManagerMock.Setup(x => x.TraerTodosHedgeMaterial(fecha, fecha)).Returns(new List<HedgeMaterialDto>());
-            reportesManagerMock.Setup(x => x.TraerAgenteDeCompra(fecha, fecha)).Returns(new List<AgenteCompraDto>());
-            var resultado = target.PartialReporteCompraNet("26-10-2018", "26-10-2018") as PartialViewResult;
-
-            Assert.NotNull(resultado);
-        }
-
-        [Test]
-        public void DetalleExcelTest()
-        {
-            var fecha = new DateTime(2018, 08, 26);
-            var reportes = new ParamReportes() { ComercialActual = 1 };
-            reportesManagerMock.Setup(x => x.DetallePosicion(1, 10, 2018, fecha, fecha, null)).Returns(new ExcelDetallePosicionDto()
+            reportesManagerMock.Setup(x => x.TraerDatosIniciales(GlobalVariables.IdActiveDirectory)).Returns(new DatosInicialesReportes
             {
-                Headers = new string[] { "1", "2" },
-                Data = new List<string[]>() { new string[] { "a", "b" } },
-                Name = "B",
-                SheetName = "C"
+                mat = new List<MaterialesQry>(){new MaterialesQry()},
+                camp = new List<CampañaQry>(){new CampañaQry()},
+                come = new List<ComercialQry>(){new ComercialQry()},
+                estic = new List<EstadoICQry>(){new EstadoICQry()},
+                provs = new List<ProvinciaQry>() { new ProvinciaQry()},
+                segm = new List<SegmentacionQry>() { new SegmentacionQry()}
             });
+            var result = target.TraerDatosCombo();
 
-            var result = target.DetalleExcel(10, 2018, 1, "26-10-2018", "26-10-2018", null);
             Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":{\"mat\":[{\"MaterialId\":0,\"Descripcion\":null}],\"camp\":[{\"CampañaId\":0,\"Descripcion\":null}],\"segm\":[{\"SegmentacionId\":0,\"Descripcion\":null,\"Grupo\":null}],\"come\":[{\"ComercialId\":0,\"IdActiveDirectory\":null,\"Nombre\":null,\"Apellido\":null,\"Comercial\":null,\"EmpleadorACargo\":null}],\"provs\":[{\"Provinciaid\":0,\"Nombre\":null,\"Orden\":0}],\"estic\":[{\"EstadoInformeId\":0,\"Descripcion\":null}]},\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+
+        [Test]
+        public void TraerDatosReporteComprasBarraTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1 };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerComprasBarra(reportes)).Returns(new List<ResultComprasBarrasReportes>(){ new ResultComprasBarrasReportes() { MasCl100 = 1} });
+            var result = target.TraerDatosReporteComprasBarra(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"MasTn5000\":0,\"MasTn1000\":0,\"MasTn10000\":0,\"MasTn20000\":0,\"MasTn40000\":0,\"MasTn100\":0,\"MenosTn10000\":0,\"MasCl5000\":0,\"MasCl1000\":0,\"MasCl10000\":0,\"MasCl100\":1,\"MenosCl10000\":0,\"MasCl20000\":0,\"MasCl40000\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
         }
         [Test]
-        public void ReporteComprasDelDiaTest()
+        public void TraerDatosReporteObjetivoGaugeTest()
         {
-            var fecha = new DateTime(2018, 08, 26);
-            reportesManagerMock.Setup(x => x.TraerAgenteDeCompra(fecha, fecha)).Returns(new List<AgenteCompraDto>());
-            reportesManagerMock.Setup(x => x.TraerToneladasGranoTipo(fecha, fecha)).Returns(new List<ToneladasGranoTipoDto>());
-            reportesManagerMock.Setup(x => x.TraerToneladasSojaSust(fecha, fecha)).Returns(new ReporteSojaSustDto());
-            reportesManagerMock.Setup(x => x.TraerPosicionCompras(fecha, fecha)).Returns(new List<PosicionComprasDto>());
-            reportesManagerMock.Setup(x => x.TraerMonedaCantidad(fecha, fecha)).Returns(new List<PrecioCantidadDto>());
-            reportesManagerMock.Setup(x => x.TraerTodosHedgeMaterial(fecha, fecha)).Returns(new List<HedgeMaterialDto>());
-            reportesManagerMock.Setup(x => x.TraerHedgeObjetivo(fecha, fecha)).Returns(new HedgeCargaObjetivoDto());
-            reportesManagerMock.Setup(x => x.TraerTcPromedio(fecha, fecha)).Returns(new HedgeTCPromedioDto());
             var reportes = new ParamReportes() { ComercialActual = 1 };
-            reportesManagerMock.Setup(x => x.PosicionPorMaterial(fecha, fecha)).Returns(new List<ExcelPosicionMaterialDto>());
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerObjetivosGauge(reportes)).Returns(new List<ResultObjetivoGaugeReportes>() { new ResultObjetivoGaugeReportes() { Cuit = "1" } });
+            var result = target.TraerDatosReporteObjetivoGauge(reportes);
 
-            var result = target.ReporteComprasDelDia("26-10-2018", "26-10-2018");
             Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"Cuit\":\"1\",\"razonSocial\":null,\"Objetivos\":0,\"Toneladas\":0,\"Porcentajes\":0,\"Material\":null,\"Campaña\":null,\"Mes\":null,\"Año\":null,\"Comercial\":null,\"Provincia\":null,\"Segmentación\":null}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasComprasMapaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "compras", Grafico = "mapa" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerComprasMapa(reportes)).Returns(new List<ResulIndicadores>() { new ResulIndicadores() { Cuit = 1, Grano = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"Cuit\":1,\"Tonelada\":0,\"Segmentacion\":null,\"Estado\":null,\"Grano\":1,\"Provincia\":null,\"CantidadDeCliente\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasComprasTortaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1,Indicadores = "compras", Grafico = "mapa" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerComprasTorta(reportes)).Returns(new List<ResulIndicadores>() { new ResulIndicadores() { Cuit = 1,Grano=1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":null,\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasCapacidadProductivaMapaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "capacidadproductiva", Grafico = "mapa" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerCapacidadProductivaMapa(reportes)).Returns(new List<ResulIndicadores>() { new ResulIndicadores() { Cuit = 1, Grano = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"Cuit\":1,\"Tonelada\":0,\"Segmentacion\":null,\"Estado\":null,\"Grano\":1,\"Provincia\":null,\"CantidadDeCliente\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasCapacidadProductivaBarraTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "capacidadproductiva", Grafico = "barras" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerCapacidadProductivaBarra(reportes)).Returns(new List<ResultComprasBarrasReportes>() { new ResultComprasBarrasReportes() { MasCl1000 = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"MasTn5000\":0,\"MasTn1000\":0,\"MasTn10000\":0,\"MasTn20000\":0,\"MasTn40000\":0,\"MasTn100\":0,\"MenosTn10000\":0,\"MasCl5000\":0,\"MasCl1000\":1,\"MasCl10000\":0,\"MasCl100\":0,\"MenosCl10000\":0,\"MasCl20000\":0,\"MasCl40000\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasCapacidadAcopioMapaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "capacidadacopio", Grafico = "mapa" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerCapacidadDeAcopioMapa(reportes)).Returns(new List<ResulIndicadores>() { new ResulIndicadores() { Cuit = 1, Grano = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"Cuit\":1,\"Tonelada\":0,\"Segmentacion\":null,\"Estado\":null,\"Grano\":1,\"Provincia\":null,\"CantidadDeCliente\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasCapacidadAcopioBarraTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "capacidadacopio", Grafico = "barras" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerCapacidadDeAcopioBarra(reportes)).Returns(new List<ResultComprasBarrasReportes>() { new ResultComprasBarrasReportes() { MasCl1000 = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[{\"MasTn5000\":0,\"MasTn1000\":0,\"MasTn10000\":0,\"MasTn20000\":0,\"MasTn40000\":0,\"MasTn100\":0,\"MenosTn10000\":0,\"MasCl5000\":0,\"MasCl1000\":1,\"MasCl10000\":0,\"MasCl100\":0,\"MenosCl10000\":0,\"MasCl20000\":0,\"MasCl40000\":0}],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+        [Test]
+        public void TraerDatosReporteComprasBaseDeDatosTortaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "basededatos", Grafico = "torta" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerCapacidadDeAcopioBarra(reportes)).Returns(new List<ResultComprasBarrasReportes>() { new ResultComprasBarrasReportes() { MasCl1000 = 1 } });
+            var result = target.TraerDatosReporteCompras(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":[],\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+
+        [Test]
+        public void TraerDatosGrillaBDTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "basededatos", Grafico = "torta" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TraerDatosGrillaBD(reportes)).Returns(new BaseDeDatosReturn() { graficoBaseDatos = new List<graficoBaseDatos>() { new graficoBaseDatos() { segmentacion = "A", Toneladas = 100 } }, valoresGrilla = new List<valoresGrilla>() { new valoresGrilla() { Comercial = "A" } } });
+            var result = target.TraerDatosGrillaBD(reportes);
+
+            Assert.NotNull(result);
+
+            var a = serializer.Serialize(result);
+            Assert.AreEqual(
+                "{\"ContentEncoding\":null,\"ContentType\":null,\"Data\":{\"valoresGrilla\":[{\"Cuit\":null,\"Segmentación\":null,\"Toneladas\":null,\"Estado\":null,\"Grano\":null,\"RazonSocial\":null,\"Comercial\":\"A\",\"FechaAlta\":\"\\/Date(-62135586000000)\\/\"}],\"graficoBaseDatos\":[{\"segmentacion\":\"A\",\"Toneladas\":100}]},\"JsonRequestBehavior\":1,\"MaxJsonLength\":2147483647,\"RecursionLimit\":null}",
+                a);
+        }
+
+        [Test]
+        public void ExportarIndicadoresTestComprasMapaTest()
+        {
+            var reportes = new ParamReportes() { ComercialActual = 1, Indicadores = "compras", Grafico = "mapa" };
+            homeManagerMock.Setup(x => x.TraerIdComercial(GlobalVariables.IdActiveDirectory)).Returns(1);
+            reportesManagerMock.Setup(x => x.TransformarFiltros(reportes)).Returns(reportes);
+            reportesManagerMock.Setup(x => x.TraerComprasMapaExportacion(reportes)).Returns(new List<ResultIndicadoresReportesmini>() { new ResultIndicadoresReportesmini() { Cuit = "1", Comercial = "A" } });
+            var result = target.ExportarIndicadores(reportes) ;
+            var model = result.Result as JsonResult;
+            Assert.NotNull(result);
+
+            reportesManagerMock.Verify(x => x.TransformarFiltros(It.IsAny<ParamReportes>()), Times.Once);
+            reportesManagerMock.Verify(x => x.TraerComprasMapaExportacion(It.IsAny<ParamReportes>()), Times.Once);
+
+            Assert.IsTrue(((ReportesModel)model.Data).DownloadKey != "");
         }
     }
 }
