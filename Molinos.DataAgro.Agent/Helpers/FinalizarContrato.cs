7ec89124bbac5d73c44f203c5366d85604ba81a2
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-03-22 12:12:09 -0300

    Commit: 2019-03-22 12:12:08Z

--- Molinos.DataAgro.Agent/Helpers/FinalizarContrato.cs
+++ Molinos.DataAgro.Agent/Helpers/FinalizarContrato.cs
@@ -66,7 +66,7 @@ namespace Molinos.DataAgro.Agent.Helpers
                 {
                     if (cal.StandardDeCalidadId == 2)
                     {
-                        if ((cal.Valor == 2 || cal.Valor == 3) && (cal.CalidadEspecial.Id == 4 || cal.CalidadEspecial.Id == 5))
+                        if ((cal.Valor >= 2 && cal.Valor <= 3) && (cal.CalidadEspecial.Id == 4 || cal.CalidadEspecial.Id == 5))
                         {
                             listaCalidades.Add(new ZMPES5300
                             {
