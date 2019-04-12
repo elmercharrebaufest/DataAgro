ae94d4d35d7cde96cde44704dcdb3da6df5f62a0
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-04-12 09:53:36 -0300

    Commit: 2019-04-12 09:53:36Z

--- Molinos.DataAgro.Business/Managers/ComercialManager.cs
+++ Molinos.DataAgro.Business/Managers/ComercialManager.cs
@@ -1,13 +1,16 @@
 ﻿using Autofac.Extras.NLog;
+using Molinos.DataAgro.Agent.Helpers;
 using Molinos.DataAgro.Entities.Common.Enums;
 using Molinos.DataAgro.Entities.Dto;
 using Molinos.DataAgro.Entities.Entities;
+using Molinos.DataAgro.Entities.Helpers;
 using Molinos.DataAgro.Entities.Validations;
 using Molinos.DataAgro.Interfaces;
 using Molinos.DataAgro.Repository;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity;
 using System.DirectoryServices.AccountManagement;
 using System.Linq;
 
