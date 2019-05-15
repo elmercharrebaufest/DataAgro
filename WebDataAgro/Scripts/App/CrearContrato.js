c3feab5ae6e190e8fa2879f37f06d9d58caba231
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-05-15 12:08:56 -0300

    Commit: 2019-05-15 12:08:56Z

--- WebDataAgro/Scripts/App/CrearContrato.js
+++ WebDataAgro/Scripts/App/CrearContrato.js
@@ -986,9 +986,10 @@ function InicializarElementos() {
                 $(".calidadesEspecialesDatos").hide();
                 LimpiarCalidades();
             }
-            if ($("#calidadesEspecialesId").data("kendoDropDownList").text() === "Grado")
-            {
+            if ($("#calidadesEspecialesId").data("kendoDropDownList").text() === "Grado") {
                 $("#valorEspecialesId").data("kendoNumericTextBox").value(2);
+            } else {
+                $("#valorEspecialesId").data("kendoNumericTextBox").value("");
             }
         }
     });
