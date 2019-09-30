2be24605de25ee758f08e4ff0bce34ec14a3c2d6
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-09-30 15:21:55 -0300

    Commit: 2019-09-30 15:21:54Z

--- Molinos.DataAgro.Business/Managers/ReportesManager.cs
+++ Molinos.DataAgro.Business/Managers/ReportesManager.cs
@@ -702,19 +702,8 @@ namespace Molinos.DataAgro.Business.Managers
                 && (centroId == 0 || centroId == x.DestinoId));
             foreach (var cont in fijaciones)
             {
-                var posicion = new DateTime();
-                if ((DateTime.DaysInMonth(cont.FechaDesde.Year, cont.FechaDesde.Month) - cont.FechaDesde.Day) >= 10)
-                {
-                    posicion = new DateTime(cont.FechaDesde.Year, cont.FechaDesde.Month, 1);
-                }
-                else if (cont.FechaDesde.AddMonths(1).Month <= cont.FechaHasta.Month)
-                {
-                    posicion = new DateTime(cont.FechaDesde.Year, cont.FechaDesde.Month, 1).AddMonths(+1);
-                }
-                else if (cont.FechaDesde.AddMonths(1).Month > cont.FechaHasta.Month)
-                {
-                    posicion = new DateTime(cont.FechaHasta.Year, cont.FechaHasta.Month, 1);
-                }
+                var posicion=new DateTime(cont.FechaDesde.Year, cont.FechaDesde.Month, 1);
+                
                 cont.ClasificacionNegocio =(fechaPosicion >= posicion && cont.CampanaMaterialId == cont.CampanaId) || (cont.CampanaMaterialId > cont.CampanaId) ? EnumClasificacionNegocio.DisponibleFijacion :                
                 cont.CampanaMaterialId == cont.CampanaId && fechaPosicion < posicion ? EnumClasificacionNegocio.ForwardFijacion :                
                 EnumClasificacionNegocio.NewCropFijacion;
@@ -742,9 +731,10 @@ namespace Molinos.DataAgro.Business.Managers
                 && (centroId == 0 || centroId == 1));
             foreach(var cont in fason)
             {
-                var mes = int.Parse(cont.Posicion.Substring(0, 2));
-                var anio = int.Parse(cont.Posicion.Substring(3, 4));
-                var posicion = new DateTime(anio, mes, 1);
+                //var mes = int.Parse(cont.Posicion.Substring(0, 2));
+                //var anio = int.Parse(cont.Posicion.Substring(3, 4));
+                //var posicion = new DateTime(anio, mes, 1);
+                var posicion = new DateTime(cont.FechaDesde.Year, cont.FechaDesde.Month, 1);
 
                 cont.ClasificacionNegocio = (fechaPosicion >= posicion && cont.CampanaMaterialId == cont.CampanaId) || (cont.CampanaMaterialId > cont.CampanaId) ? EnumClasificacionNegocio.DisponibleFijacion :
                 cont.CampanaMaterialId == cont.CampanaId && fechaPosicion < posicion ? EnumClasificacionNegocio.ForwardFijacion :
