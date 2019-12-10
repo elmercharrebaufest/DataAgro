6a3b6f16c82c122910a6b10fd8b37cb99cf59c39
EXT Olivera <oliveraa@Molinosagro.com.ar>
2019-12-10 12:35:44 -0300

    Commit: 2019-12-10 12:35:44Z

---
+++ WebDataAgro/Seguridad/AuthorizationManager.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+
+namespace WebDataAgro.Seguridad
+    public class AuthorizationManager : ServiceAuthorizationManager
+    {
+        public AuthorizationManager()
+        {
+        }
+
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            return true;
+        }
+
+    }
+}
