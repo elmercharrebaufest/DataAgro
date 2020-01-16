62fe8a55fc23ee079b6936747024c12e8b4fce7d
EXT Olivera <oliveraa@Molinosagro.com.ar>
2020-01-16 12:56:54 -0300

    Commit: 2020-01-16 12:56:54Z

--- Molinos.DataAgro.Agent/Helpers/ModificarContratoAgent.cs
+++ Molinos.DataAgro.Agent/Helpers/ModificarContratoAgent.cs
@@ -192,7 +192,7 @@ namespace Molinos.DataAgro.Agent.Helpers
                 logger.Debug("Apertura: " + contrato.AperturaPrecio);
                 var descuentoGeneralSobrePrecio = contrato.Descuentos.AsQueryable().Where(x => x.TipoPeriodoDBId == 1 && x.TipoDBId == 1).FirstOrDefault();
                 var descuentoGeneralFueraPrecio = contrato.Descuentos.AsQueryable().Where(x => x.TipoPeriodoDBId == 1 && x.TipoDBId == 2).FirstOrDefault();
-                string fechaDolarizadoString = contrato.FechaDolarizado?.ToString("yyyy-MM-dd");
+                string fechaDolarizadoString = contrato.FechaDolarizado != null ? contrato.FechaDolarizado.Value.ToString("yyyy-MM-dd"): "";
                 string pagoDiferidoString = contrato.FechaDolarizado != null ? "X" : "";
                 string sustentableString = contrato.ImporteSustentable != null && contrato.ImporteSustentable.Value != 0 ? "X" : "";
                 string noInformaSioString = contrato.NoInformaSio != null && contrato.NoInformaSio.Value ? "X" : "";
