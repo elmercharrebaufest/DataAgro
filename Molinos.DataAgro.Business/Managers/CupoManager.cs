783e966c0c176ebef8bf7b040888c2c30200f6e2
EXT Olivera <oliveraa@Molinosagro.com.ar>
2020-02-04 10:24:13 -0300

    Commit: 2020-02-04 10:24:12Z

--- Molinos.DataAgro.Business/Managers/CupoManager.cs
+++ Molinos.DataAgro.Business/Managers/CupoManager.cs
@@ -164,7 +164,11 @@ namespace Molinos.DataAgro.Business.Managers
             {
                 error.Errores.Add(new ErrorMessage(400, "CUIT Destinatario no debe estar vacío cuando elige Fasón/Préstamo Devolución"));
             }
-            if(cupo.Id == 0 && fechaHasta.HasValue && fechaHasta< cupo.FechaIngreso)
+            if (cupo.FechaIngreso.Date < DateTime.Today)
+            {
+                error.Errores.Add(new ErrorMessage(400, "La Fecha de Ingreso no debe ser una fecha menor al día de hoy"));
+            }
+            if (cupo.Id == 0 && fechaHasta.HasValue && fechaHasta< cupo.FechaIngreso)
             {
                 error.Errores.Add(new ErrorMessage(400, "La Fecha Hasta de entrega no puede ser menor a la Fecha Desde"));
             }
