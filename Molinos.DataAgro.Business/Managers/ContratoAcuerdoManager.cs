283784f33b8e63ff4acd5c4e1f6f4aaa28cfd53b
EXT Olivera <oliveraa@Molinosagro.com.ar>
2020-04-15 15:32:45 -0300

    Commit: 2020-04-15 15:32:45Z

--- Molinos.DataAgro.Business/Managers/ContratoAcuerdoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoAcuerdoManager.cs
@@ -267,7 +267,7 @@ namespace Molinos.DataAgro.Business
                 var cantidad =
                     repositorio.Listar<Contrato, double>(x => x.Cantidad, x => DbFunctions.TruncateTime(x.Fecha) == DbFunctions.TruncateTime(hoy) &&
                  (x.EstadoId == 2 || x.EstadoId == 4 || x.EstadoId == 5) && x.Id != contrato.Id && x.TipoNegocioId == 2
-                 && x.MaterialId == rango.MaterialId);
+                 && x.MaterialId == rango.MaterialId && x.ContratoAcuerdoId == null);
                 cantidad.AddRange(repositorio.Listar<ContratoAcuerdo, double>(x => x.Cantidad, x => DbFunctions.TruncateTime(x.Fecha) == DbFunctions.TruncateTime(hoy) &&
                  (x.EstadoId == 2 || x.EstadoId == 4 || x.EstadoId == 5) && x.Id != contrato.Id && x.TipoNegocioId == 6
                  && x.MaterialId == rango.MaterialId));
