0b1fb435c556e4b91acd30d57335488489b66afb
EXT Olivera <oliveraa@Molinosagro.com.ar>
2020-05-29 17:51:56 -0300

    Commit: 2020-05-29 17:51:56Z

--- Molinos.DataAgro.Agent/Helpers/StatusContratoAgent.cs
+++ Molinos.DataAgro.Agent/Helpers/StatusContratoAgent.cs
@@ -54,13 +54,15 @@ namespace Molinos.DataAgro.Agent
                     log = repositorio.Obtener<Log>(logId.Id);
                     log.Xml += valor.ToXml();
                     repositorio.GuardarCambios();
-                    int numsio;
-                    if (!string.IsNullOrEmpty(valor.EX_STATUS) ||(!string.IsNullOrEmpty(valor.EX_NUM_SIO) && (int.TryParse(valor.EX_NUM_SIO, out numsio) && numsio != 0)))
+                    int numsio = 0;
+                    int.TryParse(valor.EX_NUM_SIO, out numsio);
+                    if (!string.IsNullOrEmpty(valor.EX_STATUS) || numsio != 0)
                     {
-                    return "El contrato ya no se encuentra en slip o fue informado a SIO granos";
+                        return "El contrato ya no se encuentra en slip o fue informado a SIO granos";
                     }
                     return "";
-                }catch (Exception e)
+                }
+                catch (Exception e)
                 {
                     logger.Error(e.Message);
                     throw;
