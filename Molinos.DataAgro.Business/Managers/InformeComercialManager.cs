4802ed21bddb1d7b4391a5541fa74454603b9d67
baufest <soporte-scato@baufest.com>
2020-08-26 12:54:00 -0300

    Commit: 2020-08-26 12:54:00Z

--- Molinos.DataAgro.Business/Managers/InformeComercialManager.cs
+++ Molinos.DataAgro.Business/Managers/InformeComercialManager.cs
@@ -43,6 +43,14 @@ namespace Molinos.DataAgro.Business
         public InformeResult GrabarInformeComercial(ParamInformeComercial informe, int IdActiveDirectory, List<NuevoProduccion> nuevosCampos,
             List<NuevoAcopio> nuevosAcopios, ContactoComercial contactoComercial, string direccion, string codigoPostal, int? localidadId)
         {
+            logger.Info("GrabarInformeComercial informe" + informe.ToJson());
+            logger.Info("GrabarInformeComercial nuevosCampos" + nuevosCampos.ToJson());
+            logger.Info("GrabarInformeComercial nuevosAcopios" + nuevosAcopios.ToJson());
+            logger.Info("GrabarInformeComercial contactoComercial" + contactoComercial.ToJson());
+            logger.Info("GrabarInformeComercial direccion" + direccion ?? "");
+            logger.Info("GrabarInformeComercial codigoPostal" + codigoPostal ?? "");
+            logger.Info("GrabarInformeComercial localidadId" + localidadId ?? "");
+
             var oEntityErrors = new InformeResult();
             InformeComercial inf = new InformeComercial();
             // si de MOA Operaciones generan un nuevo informe para un proveedor nuevo
@@ -102,7 +110,7 @@ namespace Molinos.DataAgro.Business
                 else
                 {
                     var camposMaterial = repositorio.Listar<CampoMaterial>(x => x.Campo.ProveedorId == informe.ProveedorId && x.CampañaId == informe.CampañaId);
-                    var campos = repositorio.Listar<CampoMaterial,Campo>(x=>x.Campo,x => x.Campo.ProveedorId == informe.ProveedorId && x.CampañaId == informe.CampañaId);
+                    var campos = repositorio.Listar<CampoMaterial, Campo>(x => x.Campo, x => x.Campo.ProveedorId == informe.ProveedorId && x.CampañaId == informe.CampañaId);
                     repositorio.RemoverTodos(camposMaterial);
                     repositorio.RemoverTodos(campos);
                     if (nuevosCampos != null)
@@ -164,6 +172,8 @@ namespace Molinos.DataAgro.Business
                     oProveedor.LocalidadId = localidadId;
                     oProveedor.Direccion = direccion;
                     oProveedor.CodigoPostal = codigoPostal;
+                    oProveedor.Telefono1 = contactoComercial.Telefono1;
+                    oProveedor.Email1 = string.IsNullOrWhiteSpace(oProveedor.Email1) ? contactoComercial.Email1 : oProveedor.Email1;
 
                     var contactos = repositorio.Listar<ContactoComercial>(x => x.ProveedorId == informe.ProveedorId);
                     repositorio.RemoverTodos(contactos);
