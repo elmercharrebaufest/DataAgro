6b1a69b748300613d0b23392ebd8c203466def9b
baufest <soporte-scato@baufest.com>
2020-09-07 17:58:38 -0300

    Commit: 2020-09-07 17:58:38Z

--- WebDataAgro/Scripts/App/ValidacionProveedor.js
+++ WebDataAgro/Scripts/App/ValidacionProveedor.js
@@ -148,17 +148,20 @@ function ValidarContactoComercial() {
     }
     var email = $('#concom-email1').val();
     for (var i = 1; i <= 3; i++) {
-        email = $('#concom-email'+i).val();
+        email = $('#concom-email' + i).val();
         if (email && email !== "") {
             if (!validateEmail(email)) {
-                MensErr("El Email "+ i +" no es válido");
+                MensErr("El Email " + i + " no es válido");
                 return false;
             }
             var mail = email.split('@');
-            if (mail[1].toLowerCase().startsWith('molinosagro')) {
-                MensErr("El Email"+i+" no debe ser de MolinosAgro");
-                return false;
+            if (AmbientePruebas != "1") {
+                if (mail[1].toLowerCase().startsWith('molinosagro')) {
+                    MensErr("El Email" + i + " no debe ser de MolinosAgro");
+                    return false;
+                }
             }
+
         }
     }
     /*
