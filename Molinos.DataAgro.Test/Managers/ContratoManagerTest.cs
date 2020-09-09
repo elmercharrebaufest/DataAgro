67696a9cee3a0a70e28b9f865c69622b119418eb
baufest <soporte-scato@baufest.com>
2020-09-09 17:53:29 -0300

    Commit: 2020-09-09 17:53:29Z

--- Molinos.DataAgro.Test/Managers/ContratoManagerTest.cs
+++ Molinos.DataAgro.Test/Managers/ContratoManagerTest.cs
@@ -909,7 +909,7 @@ namespace Molinos.DataAgro.Test.Managers
             repositorioMock.Verify(x => x.Agregar(It.IsAny<Contrato>()), Times.Never);
             repositorioMock.Verify(x => x.GuardarCambios(), Times.Never);
             Assert.IsTrue(resultado.HayError);
-            Assert.AreEqual(29, resultado.ListaErrores.Count);
+            Assert.AreEqual(28, resultado.ListaErrores.Count);
         }
 
         [Test]
