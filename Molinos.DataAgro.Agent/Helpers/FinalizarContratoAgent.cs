e31ed4654370cae882ce62bd8f36bc09e9be8f8d
baufest <soporte-scato@baufest.com>
2020-11-27 13:02:46 -0300

    Commit: 2020-11-27 13:02:46Z

--- Molinos.DataAgro.Agent/Helpers/FinalizarContratoAgent.cs
+++ Molinos.DataAgro.Agent/Helpers/FinalizarContratoAgent.cs
@@ -268,7 +268,7 @@ namespace Molinos.DataAgro.Agent.Helpers
                     IM_DESC_BONIF = listaDescuentos.ToArray(),
                     IM_CALIDAD = listaCalidades.ToArray(),
                     IM_TIPO_NEGOCIO = contrato.Madre == true ? "MADRE" : contrato.Madre == false ? "HIJO" :
-                    contrato.EsFason == true ? "FASON": contrato.PrestamoDevolucion == true ? "PRESTAMO DEVOLUCION" : contrato.TipoNegocio.Descripcion,
+                    contrato.EsFason == true ? "FASON": contrato.PrestamoDevolucion == true ? "PRESTAMO_DEVOLUCION" : contrato.TipoNegocio.Descripcion,
                     IM_APERTURA = listaApertura.ToArray()
                 };
                 logger.Debug(rq.ToXml());
