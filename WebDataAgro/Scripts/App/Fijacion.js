c79db2620990658a5ab0c783dd68aa751cedbf62
baufest <soporte-scato@baufest.com>
2020-12-17 16:23:38 -0300

    Commit: 2020-12-17 16:23:37Z

--- WebDataAgro/Scripts/App/Fijacion.js
+++ WebDataAgro/Scripts/App/Fijacion.js
@@ -3669,6 +3669,7 @@ function CargarDatosEditar(contrato, hijo) {
         $("#comercialFijacionId").data("kendoDropDownList").enable(false);
         $("#motivoOperacionAnteriorFijacion").attr("disabled", true);
         $("#fechaFijacionId").data("kendoDatePicker").enable(false);
+        $("#pizarraId").attr("disabled", true);        
          if (!tieneDolarizado || (contrato.Dolarizado == true || contrato.DolarizadoCorredor == true)) {
             $("#expressId").attr("disabled", true);
             $("#dolarizadoId").attr("disabled", true);
