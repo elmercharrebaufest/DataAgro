9fe4ea035ad7c2fa30f2c027f0a36b2bac4c3a8d
baufest <soporte-scato@baufest.com>
2021-02-18 18:12:46 -0300

    Commit: 2021-02-18 18:12:46Z

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -129,7 +129,7 @@ namespace Molinos.DataAgro.Business.Managers
             {
                 datosCombo.campaña = repositorio.Listar<Campaña, CampañaQry>(x => new CampañaQry() { CampañaId = x.CampañaId, Descripcion = x.Descripcion });
 
-                 var listaMaterial = repositorio.Listar<HabilitacionPizarra, MaterialQry>(
+                var listaMaterial = repositorio.Listar<HabilitacionPizarra, MaterialQry>(
                     x => new MaterialQry() { MaterialId = x.MaterialId, Descripcion = x.Material.Descripcion, CampaniaTableroId = x.Material.CampaniaTableroId.Value, CampañaIdActual = x.Material.CampañaId.Value },
                     x => x.DesdeVigencia <= hoy
                     && x.HastaVigencia >= hoy
