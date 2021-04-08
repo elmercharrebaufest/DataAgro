c193096aaa68643977718fedbe6f22afa4583bd4
baufest <soporte-scato@baufest.com>
2021-04-08 14:51:58 -0300

    Commit: 2021-04-08 14:51:58Z

--- Molinos.DataAgro.Repository/ConsultasEF/DevolverProveedoresConCorredor.cs
+++ Molinos.DataAgro.Repository/ConsultasEF/DevolverProveedoresConCorredor.cs
@@ -24,14 +24,16 @@ namespace Molinos.DataAgro.Repository.ConsultasEF
                             join c in contexto.Set<ContactoComercial>() on corredorProveedor.ProveedorId equals c.ProveedorId into rgs
                             from c in rgs.DefaultIfEmpty()
                             where (corredorProveedor.Corredor.CUIT.Contains(cuitCorredor))
-                            && (corredorProveedor.Proveedor.CUIT.Contains(filtro) || c.Nombres.Contains(filtro) || c.Apellido.Contains(filtro) || corredorProveedor.Proveedor.RazonSocial.Contains(filtro))
+                            && (corredorProveedor.Proveedor.CUIT.Contains(filtro) || c.Nombres.Contains(filtro) || c.Apellido.Contains(filtro) || corredorProveedor.Proveedor.Alias.Contains(filtro)
+                            || corredorProveedor.Proveedor.RazonSocial.Contains(filtro))
                             group c by corredorProveedor into provs
                             select new BusquedaHome
                             {
                                 Id = provs.Key.ProveedorId,
                                 Cuit = provs.Key.Proveedor.CUIT,
-                                RazonSocial = provs.Key.Proveedor.RazonSocial,
-                                Filtro = filtro + "|" + provs.Key.Proveedor.RazonSocial + " (" + provs.Key.Proveedor.CUIT + ")"
+                                RazonSocial = !string.IsNullOrEmpty(provs.Key.Proveedor.Alias) ? (provs.Key.Proveedor.Alias + " - " + provs.Key.Proveedor.RazonSocial) : provs.Key.Proveedor.RazonSocial,
+                                Alias = provs.Key.Proveedor.Alias,
+                                Filtro = filtro + "|" + (!string.IsNullOrEmpty(provs.Key.Proveedor.Alias) ? (provs.Key.Proveedor.Alias + " - " + provs.Key.Proveedor.RazonSocial) : provs.Key.Proveedor.RazonSocial) + " (" + provs.Key.Proveedor.CUIT + ")"
                             }).Distinct().Take(15);
 
             return resultado.ToList();
