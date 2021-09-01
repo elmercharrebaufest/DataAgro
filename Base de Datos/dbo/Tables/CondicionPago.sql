2bad3b3e7912be80cf171351f1a8fbcf2a9e0f35
baufest <soporte-scato@baufest.com>
2021-09-01 14:53:28 -0300

    Commit: 2021-09-01 14:53:27Z

--- Base de Datos/dbo/Tables/CondicionPago.sql
+++ Base de Datos/dbo/Tables/CondicionPago.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[CondicionPago] (
     [Id]	INT IDENTITY (1, 1) NOT NULL,
-    [Descripcion]         VARCHAR(50) NOT NULL,
-    [CodigoSap]           VARCHAR(20) NOT NULL
+    [Descripcion]    VARCHAR(50)  NULL,
+    [CodigoSap]      VARCHAR(20) NOT NULL
 
     CONSTRAINT [PK_CondicionPago] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
