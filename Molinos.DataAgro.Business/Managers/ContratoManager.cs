fe6e0f71711ace33047be40979a2e77b5d88e0d9
baufest <soporte-scato@baufest.com>
2021-09-14 12:18:13 -0300

    Commit: 2021-09-14 12:18:13Z

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -5309,56 +5309,9 @@ namespace Molinos.DataAgro.Business.Managers
                 return error;
             }
             return error;
-        }
+        }   
 
-        public List<NegocioAsociadoDto> DevolverContratosParaAsociar(int contratoId, string numero)
-        {
-            var aFijar = repositorio.Obtener<Contrato>(x => x.Id == contratoId);
-            if (aFijar != null)
-            {
-                var negociosQueYaEstanAsociados = DevolverContratoAsociadosPase(contratoId).Select(x => x.Id);
-                Int32.TryParse(numero, out int id);
-                var negocios = repositorio.Listar<Negocio, NegocioAsociadoDto>(
-                    x => new NegocioAsociadoDto()
-                    {
-                        Id = x.Id,
-                        TipoNegocioDesc = x.TipoNegocio.Descripcion,
-                        Precio = x.Precio,
-                        MonedaDesc = "USD",
-                        MaterialDesc = x.Material.Descripcion,
-                        Cantidad = x.Cantidad,
-                        Campania = x.Campana.Descripcion,
-                        Color = x.TipoNegocioId == 2 ? "" : "",
-                        Posicion = x.TipoNegocioId == 2 ? (SqlFunctions.DateName("day", x.HastaFijacion) + "/" + SqlFunctions.DatePart("month", x.FechaHasta) + "/" + SqlFunctions.DateName("year", x.FechaHasta)) : x.Posicion,
-                    }, x => ((x.TipoNegocioId == 2 && x.TipoAgenteCompraId == null && x.ContratoSAP.Contains(numero)) || (x.TipoNegocioId == 5 && x.Id == id)) && x.MonedaId == "USDM "
-                       && !negociosQueYaEstanAsociados.Contains(x.Id) && x.CampanaId == x.Material.CampaniaTableroId && x.MaterialId == aFijar.MaterialId);
-                return negocios;
-            }
-            return new List<NegocioAsociadoDto>();
-        }
-
-        public List<NegocioAsociadoDto> DevolverContratoAsociadosPase(int contratoSap)
-        {
-            var aFijar = repositorio.Obtener<Contrato>(x => x.Id == contratoSap);
-            if (aFijar != null)
-            {
-                var negociosQueYaEstanAsociados = repositorio.Listar<NegocioAsociado, NegocioAsociadoDto>(
-                    x => new NegocioAsociadoDto()
-                    {
-                        Id = x.Asociado.Id,
-                        TipoNegocioDesc = x.Asociado.TipoNegocio.Descripcion,
-                        Precio = x.Asociado.Precio,
-                        MonedaDesc = "USD",
-                        MaterialDesc = x.Asociado.Material.Descripcion,
-                        Cantidad = x.Asociado.Cantidad,
-                        Campania = x.Asociado.Campana.Descripcion,
-                        Color = x.Asociado.TipoNegocioId == 2 ? "" : "",
-                        Posicion = x.Asociado.TipoNegocioId == 2 ? (SqlFunctions.DateName("day", x.Asociado.HastaFijacion) + "/" + SqlFunctions.DatePart("month", x.Asociado.FechaHasta) + "/" + SqlFunctions.DateName("year", x.Asociado.FechaHasta)) : x.Asociado.Posicion,
-                    }, x => x.AFijarId == aFijar.Id);
-                return negociosQueYaEstanAsociados;
-            }
-            return new List<NegocioAsociadoDto>();
-        }
+      
         public void EnviarMailVenta(Contrato contrato, List<DescuentoBonificacion> objDescuento, List<Calidad> objCalidad, string comercial)
         {
             var lista = new List<string>();
