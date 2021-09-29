951ac9c353b7002c633a276e539d8e5ea8dab585
baufest <soporte-scato@baufest.com>
2021-09-29 14:11:46 -0300

    Commit: 2021-09-29 14:11:45Z

--- WebDataAgro/Scripts/App/Fijacion.js
+++ WebDataAgro/Scripts/App/Fijacion.js
@@ -4979,7 +4979,7 @@ function ArmarGrillaContratosPendientes() {
         data: contratos
     });
     grid.setDataSource(data);
-    setTimeout(function () { grid.setOptions({ height: 480 }) }, 200);
+    setTimeout(function () { grid.setOptions({ height: 450 }) }, 200);
     $("#modalContratosPendientes").modal("show");
     
 }
@@ -5016,7 +5016,7 @@ function inicializarGrillaContratosPendientes() {
 
 
             },
-            height: 480,
+            height: 450,
             toolbar: [{ template: kendo.template($("#template").html()) }],
             pageable: false,
             columns: [
