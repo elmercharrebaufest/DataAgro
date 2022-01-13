90c5dcdc90e399154ecdac0c1b07745031c2e0e0
baufest <soporte-scato@baufest.com>
2022-01-13 11:53:42 -0300

    Commit: 2022-01-13 11:53:41Z

--- WebDataAgro/Scripts/App/Fijacion.js
+++ WebDataAgro/Scripts/App/Fijacion.js
@@ -285,12 +285,6 @@ function ArmarAperturaDesdeAFijar(afijar) {
             $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
             $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
         }
-        if (fijacionVirtual == true && afijar.PorcentajeSobrePrecio > 0) {
-            $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").readonly();
-            $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").readonly();
-            $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
-            $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
-        }
         else {
             $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").readonly(false);
             $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "white");
@@ -301,6 +295,13 @@ function ArmarAperturaDesdeAFijar(afijar) {
                 $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "white");
                 $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").readonly(false);
             }
+        }
+
+        if (fijacionVirtual == true && afijar.PorcentajeSobrePrecio > 0) {
+            $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").readonly();
+            $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").readonly();
+            $("#aperturaPrecioImporteComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
+            $("#aperturaPrecioPorcentajeComisionesId").data("kendoNumericTextBox").wrapper.find("input").css("background-color", "lightgray");
         }
     }
     $.each(afijar.Aperturas, function (key, apertura) {
