d1bdf2a2daaa85ef75c1442caead9ab8470952c9
baufest <soporte-scato@baufest.com>
2022-01-17 18:06:01 -0300

    Commit: 2022-01-17 18:06:00Z

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -1972,8 +1972,8 @@ namespace Molinos.DataAgro.Business.Managers
             if (rango != null)
             {
                 var grupo = repositorio.Obtener<Comercial, int>(x => x.ComercialId == contrato.ComercialId, x => x.GrupoDeComprasId.Value);
-                var cantidad =
-                    repositorio.Listar<Contrato, double>(x => x.Cantidad, x => DbFunctions.TruncateTime(x.Fecha) == DbFunctions.TruncateTime(hoy) &&
+                //double cantidad = 0;
+                var cantidad =  repositorio.Listar<Contrato, double>(x => x.Cantidad, x => DbFunctions.TruncateTime(x.Fecha) == DbFunctions.TruncateTime(hoy) &&
                  (x.EstadoId == 2 || x.EstadoId == 4 || x.EstadoId == 5) && x.Id != contrato.Id && x.TipoNegocioId == 2
                  && x.MaterialId == rango.MaterialId && x.ContratoAcuerdoId == null);
                 cantidad.AddRange(repositorio.Listar<ContratoAcuerdo, double>(x => x.Cantidad, x => DbFunctions.TruncateTime(x.Fecha) == DbFunctions.TruncateTime(hoy) &&
