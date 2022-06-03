7c9025c2ca48a85e679ded933c0ecf2af6eaf3b5
TFS_Servicio <tfs>
2022-06-03 18:24:22 -0300

    Commit: 2022-06-03 18:24:21Z

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -6918,7 +6918,45 @@ namespace Molinos.DataAgro.Business.Managers
                 if (pendientes != null && pendientes.Count > 0)
                 {
                     var cantidadCartaDePorte = pendientes.Where(x => !string.IsNullOrEmpty(x.CartasPorte)).Sum(x => x.Cantidad);
-                    var cantidadDisponible = DevolverCantidadDisponible(tieneSustentable, contratosPendientes, pendientes);
+                    var cantidadContratoDeSAP = pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && (x.Canje || x.CD || x.Warrant)).Sum(x => x.KgContrato);
+                    logger.Debug($"cantidadContratoDeSAPCanje {pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && (x.Canje || x.CD || x.Warrant)).ToJson()}");
+
+                    var contratosPendientesAplicar = pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && x.Canje == false && x.CD == false && x.Warrant == false).Select(x => x.Contrato).ToList();
+                    logger.Debug($"contratosPendientesAplicar {contratosPendientesAplicar.ToJson()}");
+
+
+                    var contratosFinalizados = repositorio.Listar<Contrato, BasicoContrato>(x => new BasicoContrato
+                    {
+                        ContratoSAP = x.ContratoSAP,
+                        BoletoId = x.BoletoId,
+                        Cantidad = x.Cantidad
+                    }, x => x.BoletoId == 5 && contratosPendientesAplicar.Contains(x.ContratoSAP));
+
+                    logger.Debug($"contratosFinalizados {contratosFinalizados.ToJson()}");
+                    var cantidadContratosKilosPendientesAplicar = pendientes.Where(x => contratosFinalizados.Select(y => y.ContratoSAP).Contains(x.Contrato)).Sum(x => x.KgContrato);
+                    var cantidadNegocioPendiente = contratosPendientes.Where(x => !pendientes.Any(y => y.Contrato.Contains(x.ContratoSAP))).Sum(x => x.Cantidad);
+                    foreach (var item in contratosPendientes)
+                    {
+                        logger.Debug($"contratosPendientesEnDA {item.ContratoSAP}");
+                    }
+                    logger.Debug($"pendientes {pendientes.ToJson()}");
+
+                    logger.Debug($"cantidadContratosKilosPendientesAplicar {cantidadContratosKilosPendientesAplicar.ToJson()}");
+
+                    //Soja Comun
+                    if (tieneSustentable == false)
+                    {
+                        cantidadCartaDePorte = pendientes.Where(x => !string.IsNullOrEmpty(x.CartasPorte) && !x.Sustentable).Sum(x => x.Cantidad);
+                        cantidadContratoDeSAP = pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && !x.Sustentable && (x.Canje || x.CD || x.Warrant)).Sum(x => x.KgContrato);
+                        logger.Debug($"soja comun {pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && !x.Sustentable && (x.Canje || x.CD || x.Warrant)).ToJson()}");
+                        cantidadNegocioPendiente = contratosPendientes.Where(x => x.Sustentable != true).Sum(x => x.Cantidad);
+                        cantidadContratosKilosPendientesAplicar = pendientes.Where(x => x.Sustentable != true && contratosFinalizados.Select(y => y.ContratoSAP).Contains(x.Contrato)).Sum(x => x.KgContrato);
+                    }
+                    logger.Debug($"soja sustentable {pendientes.Where(x => !string.IsNullOrEmpty(x.Contrato) && (x.Canje || x.CD || x.Warrant)).ToJson()}");
+                    logger.Debug($"CantidadCartaDePorte {cantidadCartaDePorte}, cantidadContratoDeSAP {cantidadContratoDeSAP}, cantidadNegocioPendiente {cantidadNegocioPendiente}, cantidadContratosKilosPendientesAplicar {cantidadContratosKilosPendientesAplicar}");
+                    var cantidadDisponible = cantidadCartaDePorte - cantidadContratoDeSAP - (decimal)cantidadNegocioPendiente - cantidadContratosKilosPendientesAplicar;
+                    //return cantidadDisponible;
+                    //var cantidadDisponible = DevolverCantidadDisponible(tieneSustentable, contratosPendientes, pendientes);
                     disponible.CantidadDisponible = cantidadDisponible < 0 ? 0 : cantidadDisponible;
                     disponible.CantidadTotal = cantidadCartaDePorte;
                 }
