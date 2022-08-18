eb69e98ae6fe270d08461bc6c201ba51f702b103
TFS_Servicio <tfs>
2022-08-18 15:54:19 -0300

    Commit: 2022-08-18 15:54:17Z

--- Molinos.DataAgro.Business/Managers/ComprasManager.cs
+++ Molinos.DataAgro.Business/Managers/ComprasManager.cs
@@ -203,7 +203,7 @@ namespace Molinos.DataAgro.Business.Managers
 
             var histActual = repositorio.Listar<CampanaMaterialDetallePorMes>();
             var proveedores = repositorio.Listar<Proveedor, ProveedorBasicoDto>(x => new ProveedorBasicoDto { ProveedorId = x.ProveedorId, CUIT = x.CUIT }).GroupBy(x => x.CUIT.ToUpper().Trim()).ToDictionary(x => x.Key);
-            var corredores = repositorio.Listar<Proveedor, ProveedorBasicoDto>(x => new ProveedorBasicoDto { ProveedorId = x.ProveedorId, CUIT = x.CUIT }).ToList();
+            var corredores = repositorio.Listar<Proveedor, ProveedorBasicoDto>(x => new ProveedorBasicoDto { ProveedorId = x.ProveedorId, CUIT = x.CUIT },x=>x.SegmentacionId== 5 || x.SegmentacionId==7).ToList();
             var campanias = repositorio.Listar<Campaña, CampañaDto>(x => new CampañaDto { CampañaId = x.CampañaId, Descripcion = x.Descripcion }).ToDictionary(x => x.Descripcion.ToUpper().Trim());
             var materiales = repositorio.Listar<Material, MaterialBasicoDto>(x => new MaterialBasicoDto { MaterialId = x.MaterialId, Codigo = x.Codigo }).ToDictionary(x => x.Codigo.ToUpper().Trim());
             var campaniaMaterialPorMes = new List<CampanaMaterialDetallePorMes>();
