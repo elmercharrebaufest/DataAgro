c6a36aa3b06736d37bc9fa3e01dcae6fb3a2ec84
TFS_Servicio <tfs>
2023-06-28 10:24:07 -0300

    Commit: 2023-06-28 10:24:06Z

--- WebDataAgro/Controllers/HomeController.cs
+++ WebDataAgro/Controllers/HomeController.cs
@@ -64,7 +64,22 @@ namespace WebDataAgro.Controllers
                 ComercialId = GlobalVariables.ComercialId,
                 Equipo = GlobalVariables.Equipo
             };
-            var equipo = PermisosHelper.Is(PermisosDataAgro.VerTodos) ? GlobalVariables.EquipoReal : PermisosHelper.Is(PermisosDataAgro.ProveedorZonaPropia) ? mobjHomeManager.ListarTodosLosComercialesConMismaZona(GlobalVariables.ComercialId) : GlobalVariables.Equipo;
+            logger.Debug($"inicializar GlobalVariables.ComercialId:{GlobalVariables.ComercialId}, usuairo: {User.Identity.Name}");
+            logger.Debug($"PermisosHelper.Is(PermisosDataAgro.VerTodos): {PermisosHelper.Is(PermisosDataAgro.VerTodos)}");
+            logger.Debug($"GlobalVariables.EquipoReal: {GlobalVariables.EquipoReal}");
+            logger.Debug($"GlobalVariables.Equipol: {GlobalVariables.Equipo}");
+            logger.Debug($"PermisosHelper.Is(PermisosDataAgro.ProveedorZonaPropia): {PermisosHelper.Is(PermisosDataAgro.ProveedorZonaPropia)}");
+            logger.Debug($"ListarTodosLosComercialesConMismaZona: {mobjHomeManager.ListarTodosLosComercialesConMismaZona(GlobalVariables.ComercialId)}");
+
+
+            var verTodos = PermisosHelper.Is(PermisosDataAgro.VerTodos);
+            var proveedorZonaPropia = PermisosHelper.Is(PermisosDataAgro.ProveedorZonaPropia);
+            var comercialesConMismaZona = mobjHomeManager.ListarTodosLosComercialesConMismaZona(GlobalVariables.ComercialId);
+
+            var equipo = verTodos ? GlobalVariables.EquipoReal : 
+                proveedorZonaPropia ? comercialesConMismaZona : 
+                GlobalVariables.Equipo;
+
             var result = mobjHomeManager.TraerBusquedaContacto(filtro, 1, equipo);
 
             model.Objetivo = mobjHomeManager.TraerInfoObjetivo(comercialId, equipo, zonaId, GlobalVariables.ComercialId);
