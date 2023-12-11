b8e99ab0ceb836cd28088ecb954f20651ba8301a
Gonzalo Sian <gsian@baufest.com>
2023-12-11 16:07:20 -0300

    DAT100-2564 - BACK Research . Sincro con PoweApp
    Researh - Datos Satélites

---
+++ Base de Datos/dbo/Tables/ResearchCondicion.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ResearchCondicion]
+(
+	[CondicionId] INT IDENTITY (1, 1) NOT NULL,
+	[Descripcion] VARCHAR (50) NULL,
+	CONSTRAINT [PK_ResearchCondicion] PRIMARY KEY CLUSTERED ([CondicionId] ASC),
+)
---
+++ Base de Datos/dbo/Tables/ResearchHumedadSuelo.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ResearchHumedadSuelo]
+(
+	[HumedadSueloId] INT IDENTITY (1, 1) NOT NULL,
+	[Descripcion] VARCHAR (50) NULL,
+	CONSTRAINT [PK_ResearchHumedadSuelo] PRIMARY KEY CLUSTERED ([HumedadSueloId] ASC),
+)
