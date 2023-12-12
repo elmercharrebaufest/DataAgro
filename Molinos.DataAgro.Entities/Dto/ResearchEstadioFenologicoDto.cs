d4f3e487fb33f08172ac74bcc2bf106c20eaf3a9
Gonzalo Sian <gsian@baufest.com>
2023-12-12 18:42:51 -0300

    DAT100-2567 - Researh - Datos Satélites
    DAT100-2567 - Researh - Datos Satélites

---
+++ Molinos.DataAgro.Entities/Dto/ResearchEstadioFenologicoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.DataAgro.Entities.Dto
+{
+    public partial class ResearchEstadioFenologicoDto
+    {
+        public int EstadioFenologicoId { get; set; }
+        public int MaterialId { get; set; }
+        public int EstadioId { get; set; }
+        public bool ConRendimiento { get; set; }
+    }
+}
---
+++ Molinos.DataAgro.Entities/Entities/ResearchTipoCarga.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Molinos.DataAgro.Entities.Entities
+{
+    public partial class ResearchTipoCarga
+    {
+        [Key]
+        public int TipoCargaId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
