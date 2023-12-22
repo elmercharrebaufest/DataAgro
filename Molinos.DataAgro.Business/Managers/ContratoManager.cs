19dc042d5895492911e48c7ae5ac82200e919721
Gonzalo Sian <gsian@baufest.com>
2023-12-22 12:56:31 -0300

    DAT100-2616 - Fix. Guarda muchos servicios y genera duplicados en SAP
    DAT100-2616 - Se guardan todos los servicios y viaja mal a SAP, lo que genera contratos duplicados

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -1697,6 +1697,17 @@ namespace Molinos.DataAgro.Business.Managers
         {
             var oEntityErrors = new GrabarContratoResult();
 
+            var listaServicioValor = repositorio.Listar<ServicioValor>();
+            List<int> listaFiltradaServicioValor = new List<int>();
+            if (listaServicioValor != null)
+            {
+                List<ServicioValor> listaFiltrada = listaServicioValor
+                    .Where(x => x.MaterialId == oContrato.MaterialId && x.CentroId == oContrato.DestinoId)
+                    .ToList();
+
+                listaFiltrada.ForEach(x => listaFiltradaServicioValor.Add(x.Id));
+            }
+
             Validar(oContrato, oEntityErrors, false);
 
             if (oEntityErrors.Errores.Count > 0)
@@ -1784,6 +1795,8 @@ namespace Molinos.DataAgro.Business.Managers
                 {
                     logger.Debug($"GrabarServicioModificado() para: id {oContrato.Id}");
                     GrabarServicioModificado(oContrato.Servicios.ToList(), oContrato.MaterialId, oContrato.DestinoId ?? 0);
+
+                    oContrato.Servicios = oContrato.Servicios.Where(x => listaFiltradaServicioValor.Contains(x.ServicioValorId)).ToList();
                 }
 
                 if ((oContratoSave.Precio != oContrato.Precio || oContratoSave.Cantidad != oContrato.Cantidad || oContratoSave.DesdeFijacion != oContrato.DesdeFijacion ||
@@ -1822,6 +1835,8 @@ namespace Molinos.DataAgro.Business.Managers
                 {
                     logger.Debug($"GrabarServicioModificado() para: id {oContrato.Id}, proveedor {oContrato.ProveedorId}, destino {oContrato.DestinoId}, fecha-desde {oContrato.FechaDesde}, cantidad {oContrato.Cantidad}, comercial {oContrato.ComercialId}");
                     GrabarServicioModificado(oContrato.Servicios.ToList(), oContrato.MaterialId, oContrato.DestinoId ?? 0);
+
+                    oContrato.Servicios = oContrato.Servicios.Where(x => listaFiltradaServicioValor.Contains(x.ServicioValorId)).ToList();
                 }
             }
 
@@ -2081,6 +2096,8 @@ namespace Molinos.DataAgro.Business.Managers
             {
                 logger.Debug($"GrabarServicioModificado() para: id {oContrato.Id}, proveedor {oContrato.ProveedorId}, destino {oContrato.DestinoId}, fecha-desde {oContrato.FechaDesde}, cantidad {oContrato.Cantidad}, comercial {oContrato.ComercialId}");
                 GrabarServicioModificado(oContrato.Servicios.ToList(), oContrato.MaterialId, oContrato.DestinoId ?? 0);
+
+                oContrato.Servicios = oContrato.Servicios.Where(x => listaFiltradaServicioValor.Contains(x.ServicioValorId)).ToList();
             }
 
             if (oContratoSave.ContratoSAP != null)
@@ -8725,6 +8742,7 @@ namespace Molinos.DataAgro.Business.Managers
 
         private void GrabarServicioModificado(List<Servicio> servicios, int materialid, int centroId)
         {
+            logger.Debug($"Servicios - Material {materialid} - Centro {centroId}");
             var serviciosMaestro = TraerTodoServicio(materialid, centroId);
             if (servicios != null && servicios.Count > 0)
             {
