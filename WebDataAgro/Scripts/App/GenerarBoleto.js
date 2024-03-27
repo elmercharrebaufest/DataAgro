ce1f75742d139c167d808b42f2a6f22d6de45ece
EXT Paz, Cristian Jose <crpaz@baufest.com>
2024-03-27 17:19:33 -0300

    Merged PR 305: DAT100-2800-Boleto-filtrar-por-fechas-contratos-fijaciones-update
    Reduccion de codigo a un archivo

--- WebDataAgro/Scripts/App/GenerarBoleto.js
+++ WebDataAgro/Scripts/App/GenerarBoleto.js
@@ -65,3 +65,24 @@ function ValidarGenerarBoletos() {
 
     return true;
 }
+
+$("body").on("click", "#filtrarBoletos", function () {
+    FiltrarBoletos();
+});
+
+function FiltrarBoletos() {
+    let fechaDesde = $("#desde").val();
+    let fechaHasta = $("#hasta").val();
+    let tipoNegocio = $("#tipoId").val();
+    BlockUi("Consultando...");
+    var data = { desde: fechaDesde, hasta: fechaHasta, negocio: tipoNegocio };
+    result = MSExecuteOnServer('/Boleto/FiltrarBoletos', data);
+    $.unblockUI();
+    if (result.length == 0) {
+        MensAlerta("Sin Resultados");
+    } else {
+        var concatenada = result.join(';');
+        $("#ContratoSAPId").val(concatenada);
+        CambioVariosContratos();
+    }
+}
