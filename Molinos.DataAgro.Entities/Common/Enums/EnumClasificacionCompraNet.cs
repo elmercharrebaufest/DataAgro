0dc91d7f41e4b9b424b104da7b41df538a6a3f7a
EXT Lusi, Francisco <flusi@baufest.com>
2024-04-17 12:22:51 -0300

    Merged PR 371: DAT100-2830--Revisar-Clausulas-Faltantes--boletos
    Modificar logica de condicion clausula 44

---
+++ Molinos.DataAgro.Entities/Common/Enums/EnumClasificacionCompraNet.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Molinos.DataAgro.Entities.Common.Enums
+{
+    public enum EnumClasificacionCompraNet
+    {
+        Productor = 1,
+        Acopiador = 2,
+        Otros = 3,
+    }
+}
