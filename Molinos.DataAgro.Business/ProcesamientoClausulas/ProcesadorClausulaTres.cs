bba989b6d3ec7df450bab68637a270dd3fec4c0f
Cristian José Paz <crpaz@baufest.com>
2024-06-04 13:15:26 -0300

    Optimizacion

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaTres.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaTres.cs
@@ -1,13 +1,12 @@
 ﻿using Autofac.Extras.NLog;
+using Humanizer;
+using Molinos.DataAgro.Entities.Common.Enums;
 using Molinos.DataAgro.Entities.Dto;
 using Molinos.DataAgro.Entities.Entities;
+using Molinos.DataAgro.Interfaces;
 using Molinos.DataAgro.Repository;
 using System;
-using Humanizer;
 using System.Globalization;
-using Molinos.DataAgro.Interfaces;
-using Molinos.DataAgro.Entities.Common.Enums;
-using System.Diagnostics.Contracts;
 
 namespace Molinos.DataAgro.Business.Procesamiento
 {
