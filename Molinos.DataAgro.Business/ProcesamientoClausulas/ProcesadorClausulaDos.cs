a48d2c7f73f3725350c2bf7055adfe23fefd0a46
EXT Brisa Giselle, Melgarejo <Brisa.Melgarejo@molinosagro.com.ar>
2024-09-20 15:20:53 -0300

    Merged PR 841: DAT100-3099 - Agregar validacion clausula para confirma
    DAT100-3099 - Agregar validacion clausula para confirma
    
    - Agregue una validacion para que las clausualas CuarentaYSiete y la Dos solo se agreguen si el boleto no es confirma.

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDos.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDos.cs
@@ -3,6 +3,7 @@ using Molinos.DataAgro.Entities.Dto;
 using Molinos.DataAgro.Entities.Entities;
 using Molinos.DataAgro.Repository;
 using Molinos.DataAgro.Interfaces;
+using Molinos.DataAgro.Entities.Common.Enums;
 
 namespace Molinos.DataAgro.Business.Procesamiento
 {
@@ -16,18 +17,19 @@ namespace Molinos.DataAgro.Business.Procesamiento
 
         public override ResultadoClausula DevolverClausulas(ClausulaDos clausula)
         {
-            // CLAUSULA SIEMPRE PRESENTE
-
             var res = new ResultadoClausula();
-            res.Texto += $"Las entregas y recibos se efectuarán desde el {clausula.Basico.FechaDesde.GetValueOrDefault():dd'/'MM'/'yyyy} hasta el {clausula.Basico.FechaHasta.GetValueOrDefault():dd'/'MM'/'yyyy}";
-            if (clausula.Basico.MercsDeposito == true)
+            if (clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.CONFIRMA)
             {
-                res.Texto += $" habiendo a su vez mercadería descargada";
-            }
-            res.Texto += $", haciéndose el recibo por el comprador en planta {ReemplazarSanLorenzo(clausula.Basico.DestinoDescripcion)}, Localidad {clausula.Basico.DestinoLocalidad}, " +
-                $"de Provincia de {clausula.Basico.DestinoProvincia}. Queda establecido que toda tasa contribución, impuesto provincial y/o municipal que grave " +
-                $"la presente operación será a cargo de la parte vendedora.";
 
+                res.Texto += $"Las entregas y recibos se efectuarán desde el {clausula.Basico.FechaDesde.GetValueOrDefault():dd'/'MM'/'yyyy} hasta el {clausula.Basico.FechaHasta.GetValueOrDefault():dd'/'MM'/'yyyy}";
+                if (clausula.Basico.MercsDeposito == true)
+                {
+                    res.Texto += $" habiendo a su vez mercadería descargada";
+                }
+                res.Texto += $", haciéndose el recibo por el comprador en planta {ReemplazarSanLorenzo(clausula.Basico.DestinoDescripcion)}, Localidad {clausula.Basico.DestinoLocalidad}, " +
+                    $"de Provincia de {clausula.Basico.DestinoProvincia}. Queda establecido que toda tasa contribución, impuesto provincial y/o municipal que grave " +
+                    $"la presente operación será a cargo de la parte vendedora.";
+            }
             return res;
         }
 
