e17f60a804af793c6e9ae9eb830b4e671020690a
Cristian José Paz <crpaz@baufest.com>
2024-09-24 14:48:11 -0300

    DAT100-3087-Previsualizacion-de-Confirmas-filtrados - Grilla funcional realizada

--- Molinos.DataAgro.Interfaces/Managers/IConfirmaManager.cs
+++ Molinos.DataAgro.Interfaces/Managers/IConfirmaManager.cs
@@ -1,4 +1,5 @@
-﻿using Molinos.DataAgro.Entities.Dto;
+﻿using Kendo.DynamicLinq;
+using Molinos.DataAgro.Entities.Dto;
 using System;
 using System.Collections.Generic;
 
@@ -7,10 +8,6 @@ namespace Molinos.DataAgro.Interfaces
     public interface IConfirmaManager
     {
         DatosIniContrato TraerDatosCombos();
-        List<string> ListarNegociosPorRangoCodigoSAP(int negocioDesde, int negocioHasta, int tipoNegocio, List<int> equipo);
-        List<string> ValidarNegocios(List<string> codigosSAP, int claseNegocio, List<int> equipo);
-        string ValidarNegocio(string codigoSAP, int tipoNegocio, List<int> equipo);
-        List<string> FiltrarNegociosPorFecha(string desde, string hasta, int tipoNegocio, List<int> equipo);
         ConfirmaResult GrabarConfirmas(int claseNegocio, int ComercialId, List<string> codigosSap, bool usarWebServiceConfirma, List<int> equipo);
         string GenerarNombreArchivoConfirma(string codigoSAP);
         byte[] ObtenerArchivoXML(string nombreArchivo);
@@ -18,5 +15,6 @@ namespace Molinos.DataAgro.Interfaces
         List<ConfirmaArchivoDto> ListarConfirmas();
         List<ResultadoClausula> ObtenerClausulas(BasicoContrato basico);
         string CorregirFormatoFecha(string cadena);
+        DataSourceResult TraerNegociosFiltrados(DataSourceRequest filtro);
     }
 }
