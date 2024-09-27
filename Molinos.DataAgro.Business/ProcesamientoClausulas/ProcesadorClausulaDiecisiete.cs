f25d310bbaa22796faa8f22c67e28d44cea08bca
Gonzalo Sian <gsian@baufest.com>
2024-09-27 16:02:54 -0300

    DAT100-3122 - Bug: error al traer cláusula 17
    Cláusula 17 no deberíá salir cuando es EsFason o PrestamoDevolucion, ya que la rfc no trae la condición de fijación necesaria para la cláusula

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDiecisiete.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDiecisiete.cs
@@ -25,7 +25,7 @@ namespace Molinos.DataAgro.Business.Procesamiento
             var condiciones = datosBoleto.CondicionFijacion.FirstOrDefault();
 
             //SI EL NEGOCIO ES DE TIPO A FIJAR Y NO ES POSICIÓN PASE
-            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && (!clausula.Basico.TipoPosicionCBOTId.HasValue || clausula.Basico.TipoPosicionCBOTId.Value != 3))
+            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && (!clausula.Basico.TipoPosicionCBOTId.HasValue || clausula.Basico.TipoPosicionCBOTId.Value != (int)EnumTipoPosicionCBOT.PASE) && !(clausula.Basico.EsFason ?? false) && !clausula.Basico.PrestamoDevolucion)
             {
                 res.Texto += $"El precio de la mercadería objeto del presente contrato, se fijará cualquier día hábil a elección del vendedor. " +
                     $"El vendedor comunicará al comprador el día elegido para la fijación de precio por {clausula.Basico.CondicionFijacionDescripcion} desde el " +
@@ -34,7 +34,7 @@ namespace Molinos.DataAgro.Business.Procesamiento
                     $"Únicamente a los efectos del impuesto de sellos las partes acuerdan que el precio de referencia corresponde a Pizarra Rosario.";
             }
             //SI EL NEGOCIO ES DE TIPO A FIJAR Y ES POSICIÓN PASE
-            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && clausula.Basico.TipoPosicionCBOTId.HasValue && clausula.Basico.TipoPosicionCBOTId.Value == 3)
+            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && clausula.Basico.TipoPosicionCBOTId.HasValue && clausula.Basico.TipoPosicionCBOTId.Value == (int)EnumTipoPosicionCBOT.PASE && !(clausula.Basico.EsFason ?? false) && !clausula.Basico.PrestamoDevolucion)
             {
                 if (clausula.Basico.ImporteBonificacion.HasValue && clausula.Basico.ImporteBonificacion.Value != 0)
                 {
