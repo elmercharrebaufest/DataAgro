4e377d7156390488a24300f101500e30994725db
EXT Sian Gonzalo <gsian@baufest.com>
2024-10-01 08:41:19 -0300

    Merged PR 901: Merged PR 856: DAT100-3117
    Merged PR 856: DAT100-3117
    
    DAT100-3117
    
    - Agreugue el precio neto en la proyeccion del basico contrato que es el json que se graba para ir guardando los logs de dataagro

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -3234,6 +3234,7 @@ namespace Molinos.DataAgro.Business.Managers
                 Cantidad = x.Cantidad,
                 Ampliaciones = x.Ampliaciones,
                 Precio = x.Precio,
+                PrecioNeto = x.PrecioNeto,
                 MonedaId = x.MonedaId,
                 CampanaId = x.CampanaId ?? 0,
                 ProvinciaId = x.ProvinciaId,
