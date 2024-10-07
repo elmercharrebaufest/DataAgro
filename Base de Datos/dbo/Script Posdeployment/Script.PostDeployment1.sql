bf4cd97da608622622a176203f784459a1d20576
EXT Paz, Cristian Jose <crpaz@baufest.com>
2024-10-07 16:11:02 -0300

    Merged PR 927: DAT100-3144-Insercion-Clausula-para-Todos-Boletos

--- Base de Datos/dbo/Script Posdeployment/Script.PostDeployment1.sql
+++ Base de Datos/dbo/Script Posdeployment/Script.PostDeployment1.sql
@@ -342,6 +342,7 @@ IF NOT EXISTS (select 1 from Clausula where Discriminator = 'ClausulaCuarentaYNu
 IF NOT EXISTS (select 1 from Clausula where Discriminator = 'ClausulaCincuenta') BEGIN insert into Clausula(Discriminator, Orden, Estado) values ('ClausulaCincuenta',50 , 1); END
 IF NOT EXISTS (select 1 from Clausula where Discriminator = 'ClausulaCincuentaYUno') BEGIN insert into Clausula(Discriminator, Orden, Estado) values ('ClausulaCincuentaYUno',51 , 1); END
 IF NOT EXISTS (select 1 from Clausula where Discriminator = 'ClausulaCincuentaYDos') BEGIN insert into Clausula(Discriminator, Orden, Estado) values ('ClausulaCincuentaYDos',52 , 1); END
+IF NOT EXISTS (select 1 from Clausula where Discriminator = 'ClausulaCincuentaYTres') BEGIN insert into Clausula(Discriminator, Orden, Estado) values ('ClausulaCincuentaYTres',53 , 1); END
 
 --EstadoHome
 IF NOT EXISTS (select 1 from EstadoHome where Descripcion = 'Habilitado') BEGIN insert into EstadoHome (Descripcion, Color) values ('Habilitado', 'green'); END
