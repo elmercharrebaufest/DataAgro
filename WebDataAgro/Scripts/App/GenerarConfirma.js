c3bdef8dc02b71409c847b96babd155238399f19
Cristian José Paz <crpaz@baufest.com>
2024-10-10 00:18:28 -0300

    DAT100-3132-Grilla-Generacion-XML-Agregar-columnas-Fix-Icono-2

--- WebDataAgro/Scripts/App/GenerarConfirma.js
+++ WebDataAgro/Scripts/App/GenerarConfirma.js
@@ -171,7 +171,7 @@ function inicializarGrillaContratos() {
                             <img style="width:16px;height:16px;" src="/Content/Images/agregar-tel-mail.png" hidden>
                         </a>
                         <a onclick="GestionarClausulas('#= NegocioSAP #')" title="Gestionar Clausulas">
-                            <img style="width:16px;height:16px;" src="/Content/Images/pencil_line.png">
+                            <img style="width:16px;height:16px;" src="/Content/Images/pencil-line.png">
                         </a>
                     </div>
                 `,
