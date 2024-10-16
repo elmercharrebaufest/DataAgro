c8907b4f611a874fb2474ab0be35d83ac5eeae17
EXT Paz, Cristian Jose <crpaz@baufest.com>
2024-10-16 14:14:24 -0300

    Merged PR 971: DAT100-3135-Modificación-cláusulas-Carta-Oferta-Físico-Ajuste
    DAT100-3135-Modificación-cláusulas-Carta-Oferta-Físico-Ajuste

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYTres.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYTres.cs
@@ -17,7 +17,7 @@ namespace Molinos.DataAgro.Business.Procesamiento
         public override ResultadoClausula DevolverClausulas(ClausulaCincuentaYTres clausula)
         {
             var res = new ResultadoClausula();
-            if (clausula.Basico.PorcentajeDePago.HasValue)
+            if (clausula.Basico.PorcentajeDePago.HasValue && (clausula.Basico.CD==true || clausula.Basico.Warrant == true))
             {
                 res.Texto += $"El pago del {clausula.Basico.PorcentajeDePago.Value}% será efectuado por el Comprador en forma anticipada a la entrega de la mercadería. A dichos fines, el Vendedor se constituye en depositario de {clausula.Basico.Cantidad} kilos/toneladas de {clausula.Basico.Material}, obligándose a guardarla en su depósito y/o planta ubicada en {clausula.Basico.Provincia}, {clausula.Basico.Localidad}, en forma gratuita y a disposición del Comprador, reconociendo en cabeza de éste la propiedad de la mercadería, en atención al pago anticipado efectuado. Una vez abonada la mercadería, no podrá ser retirada de su lugar de guarda, hasta la fecha de entrega pactada en el presente contrato y/o hasta que sea indicado por el Comprador. La pérdida de la mercadería por cualquier causa que fuera y/o la falta de entrega de la misma en los plazos pactados en el contrato, será considerada causal de incumplimiento, por lo que el Comprador estará facultado para reclamar al Vendedor el monto cobrado anticipadamente con más los intereses que correspondan, y los daños y perjuicios que se hubieren generado por el incumplimiento mencionado.";
             }
