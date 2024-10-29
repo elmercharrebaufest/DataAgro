a1416ac1b2c5f1096fdc6caef4e424669ce87669
Dani Rodriguez Palacio <drodriguez@baufest.com>
2024-10-29 16:27:04 -0300

    Formato CR LF

--- Molinos.DataAgro.Business/Managers/ContratoManager.cs
+++ Molinos.DataAgro.Business/Managers/ContratoManager.cs
@@ -16,7 +16,6 @@ using System.Configuration;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.SqlServer;
-using System.Diagnostics.Contracts;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
@@ -1752,7 +1751,7 @@ namespace Molinos.DataAgro.Business.Managers
             oContratoSave.FechaDesde = oContrato.FechaDesde;
             oContratoSave.FechaHasta = oContrato.FechaHasta;
             oContratoSave.ProveedorId = oContrato.ProveedorId;
-            oContratoSave.MonedaId = oContrato.MonedaId??(oContrato.TipoNegocioId==(int)EnumTipoNegocio.A_FIJAR?"USDM ":null);
+            oContratoSave.MonedaId = oContrato.MonedaId ?? (oContrato.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR ? "USDM " : null);
             oContratoSave.GrupoCompra = oContrato.GrupoCompra;
             oContratoSave.ComercialId = oContrato.ComercialId;
             oContratoSave.LocalidadId = oContrato.LocalidadId;
