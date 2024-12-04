5f01f0897a8f981c4265b270fc95362b21ab7b83
Dani Rodriguez Palacio <drodriguez@baufest.com>
2024-12-04 16:10:00 -0300

    DAT100-3392 - Precio en mail fijación
    En el cuerpo del mail de fijaciones se muestra el precio base en la fila Precio. El neto se muestra en Observaciones.

--- Molinos.DataAgro.Business/Managers/ProveedorManager.cs
+++ Molinos.DataAgro.Business/Managers/ProveedorManager.cs
@@ -1082,26 +1082,9 @@ namespace Molinos.DataAgro.Business.Managers
             {
                 htmlBody += "Pizarra";
             }
-            //else if (oFijacionDePrecioContrato.PrecioNeto.HasValue)
-            //{
-            //    htmlBody += Split(oFijacionDePrecioContrato.PrecioNeto.Value.ToString("N2", CultureInfo.CreateSpecificCulture("es-AR"))) + " " + oFijacionDePrecioContrato.Moneda.Descripcion.ToUpper();
-            //}
             else
             {
-                Decimal precio = 0;
-                AperturaPrecio aperturaPrecio = oFijacionDePrecioContrato.AperturaPrecio.Find(x => x.ConceptoAperturaPrecio.Descripcion.Contains("Comisiones"));
-                if (aperturaPrecio != null)
-                {
-                    Decimal porcentaje = aperturaPrecio.Porcentaje / 100;
-                    Decimal importe = (decimal)(aperturaPrecio.Porcentaje > 0 ? porcentaje * oFijacionDePrecioContrato.PrecioNeto / (1 + porcentaje) : aperturaPrecio.Importe);
-                    precio = oFijacionDePrecioContrato.Precio + importe;
-                }
-                else
-                {
-                    precio = oFijacionDePrecioContrato.Precio;
-                }
-                htmlBody += Split(precio.ToString("N2", CultureInfo.CreateSpecificCulture("es-AR"))) + " " + oFijacionDePrecioContrato.Moneda.Descripcion.ToUpper();
-                //htmlBody += Split(oFijacionDePrecioContrato.Precio.ToString("N2", CultureInfo.CreateSpecificCulture("es-AR"))) + " " + oFijacionDePrecioContrato.Moneda.Descripcion.ToUpper();
+                htmlBody += Split(oFijacionDePrecioContrato.Precio.ToString("N2", CultureInfo.CreateSpecificCulture("es-AR"))) + " " + oFijacionDePrecioContrato.Moneda.Descripcion.ToUpper();
             }
             htmlBody += "<tr>" + th + "OBSERVACIONES</th>" + Td(ref linea);
             if (oFijacionDePrecioContrato.PrecioNeto.HasValue)
