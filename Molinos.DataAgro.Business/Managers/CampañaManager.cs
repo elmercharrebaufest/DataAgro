7f9739ce1e0af80aed15abb4be8edd24062b4037
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-02-06 20:38:20 +0000

    Merged PR 2815: Merged PR 2814: PR: Correccion de errores con la campaña sugerida
    Merged PR 2814: PR: Correccion de errores con la campaña sugerida

--- Molinos.DataAgro.Business/Managers/CampañaManager.cs
+++ Molinos.DataAgro.Business/Managers/CampañaManager.cs
@@ -98,7 +98,7 @@ namespace Molinos.DataAgro.Business
             
             var campoMaterial = repositorio.Listar<CampoMaterial>(x => x.Campo.ProveedorId == proveedorId).OrderByDescending(x => x.CampañaId).FirstOrDefault();
             var campañaPorMaterial = repositorio.Listar<CampañaMaterial, CampañaDto>(x => new CampañaDto { CampañaId = x.Campaña.CampañaId, Descripcion = x.Campaña.Descripcion }, x => x.MaterialId == materialId && (x.CampañaId > campañaEnAdelante), 0, "CampañaId", DirOrden.Desc).Take(3).ToList();
-            var campañaIdSiguiente = campoMaterial.CampañaId + 1;
+            var campañaIdSiguiente = campoMaterial!=null? campoMaterial.CampañaId + 1 : 0 ;
             var filtrarCampaña = campañaPorMaterial.Where(x => x.CampañaId == campañaIdSiguiente).FirstOrDefault();
 
             //if (filtrarCampaña == null)
