4e7736cd963cce486e10d8ce771e55b6ac228fb7
Gonzalo Sian <gsian@baufest.com>
2025-02-14 10:36:03 -0300

    DAT-225 - Relevar tickets y preparar pase a PRD
    Se agregan índices faltantes. Se modifican tipos de datos a NVARCHAR para correcto uso de índices.

--- Base de Datos/dbo/Tables/LogDataAgro.sql
+++ Base de Datos/dbo/Tables/LogDataAgro.sql
@@ -5,14 +5,12 @@
     [Fecha] DATETIME NOT NULL, 
     [DatoModificado] NVARCHAR(MAX) NOT NULL, 
     [ClaseId] INT NOT NULL, 
-    [Clase] VARCHAR(100) NOT NULL, 
-    [AccionRealizada] VARCHAR(100) NOT NULL, 
-
+    [Clase] NVARCHAR(100) NOT NULL, 
+    [AccionRealizada] VARCHAR(100) NOT NULL,
     [Tipo] VARCHAR(100) NOT NULL, 
     [Descripcion] VARCHAR(250) NOT NULL DEFAULT '', 
     [ProveedorId] INT NULL, 
-	[CorredorId] INT NULL, 
-
+	[CorredorId] INT NULL,
     CONSTRAINT [FK_LogDataAgro_Proveedor] FOREIGN KEY (ProveedorId) REFERENCES [Proveedor]([ProveedorId]),
     CONSTRAINT [FK_LogDataAgro_Corredor] FOREIGN KEY (CorredorId) REFERENCES [Proveedor]([ProveedorId]),
     CONSTRAINT [PK_LogDataAgro] PRIMARY KEY CLUSTERED ([Id] ASC)
--- Base de Datos/dbo/Tables/TipoResearch.sql
+++ Base de Datos/dbo/Tables/TipoResearch.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[TipoResearch] (
     [Id]				INT IDENTITY (1, 1) NOT NULL,
-    [Descripcion]       VARCHAR(MAX)	NOT NULL,
+    [Descripcion]       NVARCHAR(MAX) NOT NULL,
     
     CONSTRAINT [PK_TipoResearch] PRIMARY KEY CLUSTERED ([Id] ASC),	
 );
