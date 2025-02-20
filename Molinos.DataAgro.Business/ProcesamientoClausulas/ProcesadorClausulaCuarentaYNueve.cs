e609e70b0f57b0806dd1707e2cd206580a39e18b
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-02-20 21:04:05 +0000

    Merged PR 2916:  PR : DAT-230 - Mejoras en boletos

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCuarentaYNueve.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCuarentaYNueve.cs
@@ -19,13 +19,7 @@ namespace Molinos.DataAgro.Business.Procesamiento
         {
             var res = new ResultadoClausula();
 
-            //EL MATERIAL ES SOJA Y TIENE CALIDAD ESPECIAL GRANOS VERDES
-            if (clausula.Basico.MaterialId == (int)EnumMateriales.SOJA && clausula.Basico.StandardDeCalidadId == (int)EnumStandarCalidad.ESPECIAL)
-            {
-                res.Texto += $"Granos verdes: De 0% a 20% 0 descuento. Del 20,1 en adelante se descontará 0,2% por punto porcentual excedido. " +
-                    $"Granos verdes: De 0,5% a 30% descuento 0,20. De 30,1% a 45% 0 descuento. De 45,1% a 60% descuento 0,20. Del 60,1% en adelante se descontará 0,2% por punto porcentual excedido.";
-            }
-            else if ((clausula.Basico.MaterialId == (int)EnumMateriales.MAIZ && clausula.Basico.StandardDeCalidadId == (int)EnumStandarCalidad.ESPECIAL) ||
+            if ((clausula.Basico.MaterialId == (int)EnumMateriales.MAIZ && clausula.Basico.StandardDeCalidadId == (int)EnumStandarCalidad.ESPECIAL) ||
                 (clausula.Basico.MaterialId == (int)EnumMateriales.TRIGO && clausula.Basico.StandardDeCalidadId == (int)EnumStandarCalidad.GRADO_2))
             {
                 res.Texto += $"Condición de la mercadería Grado 2: No bonifica Grado 1, no bonifica ni rebaja Grado 2, rebaja Grado 3 y demás condiciones cámara.";
