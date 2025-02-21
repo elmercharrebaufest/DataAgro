0f98312decd62ed9e93a368f37aed6f867d9510a
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-02-21 16:23:08 +0000

    Merged PR 2922: PR : DAT-230 - Modificación de Clausula de apertura de precio

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYSeis.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYSeis.cs
@@ -24,7 +24,6 @@ namespace Molinos.DataAgro.Business.ProcesamientoClausulas
             var res = new ResultadoClausula();
 
             // Se modifica por peticion de Santiago para que cuando sea posicion CBOT no se muestre estas clausulas
-            bool esPosicionCBOT = clausula.Basico.TipoPosicionCBOTId != null ? (clausula.Basico.TipoPosicionCBOTId == 1 ? true : false) : false;
             var bonificacionAperturaPrecio = clausula.Basico.AperturaPrecios != null ? clausula.Basico.AperturaPrecios.Find(x => x.ConceptoAperturaPrecioId == 4) : new AperturaPrecioDto();
 
             if (clausula.Basico.Descuentos.Count == 0 && bonificacionAperturaPrecio.Id > 0)
