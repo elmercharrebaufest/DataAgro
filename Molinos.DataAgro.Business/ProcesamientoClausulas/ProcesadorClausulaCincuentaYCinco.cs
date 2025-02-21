befd19383bd2fc620400d8166ac29f0a160ae167
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-02-21 21:26:53 +0000

    Merged PR 2931: PR : Modificacion clausula para trigos

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYCinco.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYCinco.cs
@@ -25,7 +25,7 @@ namespace Molinos.DataAgro.Business.ProcesamientoClausulas
             {
                 string productor = clausula.Basico.Proveedor;
                 string corredor = clausula.Basico.CUITCorredor;
-                res.Texto += String.Format($"Los señores {0}, CUIT N° {1}, actúan en la presente operación en carácter de corredores quedando facultados por los vendedores para fijar el precio, facturar, recibir el pago, firmar recibos de mercadería, ampliaciones y/o anulaciones y convenir eventuales prorrogas. El vendedor faculta al corredor a firmar en su nombre y representación toda la documentación necesaria para la instrumentación o formalización del presente.", productor, corredor);
+                res.Texto += String.Format("Los señores {0}, CUIT N° {1}, actúan en la presente operación en carácter de corredores quedando facultados por los vendedores para fijar el precio, facturar, recibir el pago, firmar recibos de mercadería, ampliaciones y/o anulaciones y convenir eventuales prorrogas. El vendedor faculta al corredor a firmar en su nombre y representación toda la documentación necesaria para la instrumentación o formalización del presente.", productor, corredor);
 
             }
             return res;
