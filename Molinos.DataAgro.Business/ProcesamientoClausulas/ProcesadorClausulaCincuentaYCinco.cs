62d28829a34bf9ecd6008d095c7cc8c769b3b1d1
Evangelino Miranda tecse <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-02-24 17:12:53 +0000

    Merged PR 2936: PR : Modificacion de clausulas para boleto

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYCinco.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYCinco.cs
@@ -23,7 +23,7 @@ namespace Molinos.DataAgro.Business.ProcesamientoClausulas
             var res = new ResultadoClausula();
             if (clausula.Basico.CorredorId > 0)
             {
-                string productor = clausula.Basico.Proveedor;
+                string productor = clausula.Basico.Corredor;
                 string corredor = clausula.Basico.CUITCorredor;
                 res.Texto += String.Format("Los señores {0}, CUIT N° {1}, actúan en la presente operación en carácter de corredores quedando facultados por los vendedores para fijar el precio, facturar, recibir el pago, firmar recibos de mercadería, ampliaciones y/o anulaciones y convenir eventuales prorrogas. El vendedor faculta al corredor a firmar en su nombre y representación toda la documentación necesaria para la instrumentación o formalización del presente.", productor, corredor);
 
---
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYSiete.cs
@@ -0,0 +1,32 @@
+﻿using Autofac.Extras.NLog;
+using Molinos.DataAgro.Entities.Dto;
+using Molinos.DataAgro.Entities.Entities;
+using Molinos.DataAgro.Interfaces;
+using Molinos.DataAgro.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.DataAgro.Business.ProcesamientoClausulas
+{
+    public class ProcesadorClausulaCincuentaYSiete : ProcesadorClausula<ClausulaCincuentaYSiete>
+    {
+        public ProcesadorClausulaCincuentaYSiete(IRepositorio repositorio, ILogger log, IConsultarEstadoBoletoAgent estadoBoleto) : base(repositorio, log, estadoBoleto)
+        {
+        }
+
+        public override ResultadoClausula DevolverClausulas(ClausulaCincuentaYSiete clausula)
+        {
+            var res = new ResultadoClausula();
+            if (clausula.Basico.CorredorId > 0 || clausula.Basico.CD == true)
+            {
+                string razonSocialProveedor = clausula.Basico.Corredor;
+                string razonSocialCorredor = clausula.Basico.Proveedor;
+                res.Texto += String.Format("El pago de las Parciales y Finales resultantes del presente contrato serán realizados directamente a {0} ; Los señores {1} no se encuentran autorizados a recibir pagos ni firmar recibos de mercadería.", razonSocialProveedor, razonSocialCorredor);
+            }
+            return res;
+        }
+    }
+}
