9f2f34438d723f798d1a8d45343e8755f89c2c83
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-04-09 14:44:58 +0000

    Merged PR 3264: DAT-326 - contratos confirma clausula erronea
    DAT-326 - contratos confirma clausula erronea

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYOcho.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYOcho.cs
@@ -3,11 +3,6 @@ using Molinos.DataAgro.Entities.Dto;
 using Molinos.DataAgro.Entities.Entities;
 using Molinos.DataAgro.Interfaces;
 using Molinos.DataAgro.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Molinos.DataAgro.Business.ProcesamientoClausulas
 {
