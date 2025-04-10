824b433fa2d939b4d41e17a1d5b504ae8a1cd9f8
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-04-10 19:11:05 +0000

    Merged PR 3283: Correccion clausula negocio con cd directo con corredor

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYOcho.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuentaYOcho.cs
@@ -14,7 +14,7 @@ namespace Molinos.DataAgro.Business.ProcesamientoClausulas
         public override ResultadoClausula DevolverClausulas(ClausulaCincuentaYOcho clausula)
         {
             var res = new ResultadoClausula();
-            if (clausula.Basico.CorredorId > 0 || clausula.Basico.CD == true)
+            if (clausula.Basico.CorredorId > 0 && clausula.Basico.CD == true)
             {
                 res.Texto += "En caso de incumplimiento, podrá ejecutarse la obligación mediante la entrega de la mercadería objeto del CD o, en su defecto, el importe necesario para poder adquirir la misma cantidad de mercadería objeto del boleto a la fecha de vencimiento del plazo de entrega.";
             }
