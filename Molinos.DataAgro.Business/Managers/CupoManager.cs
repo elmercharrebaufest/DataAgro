dac8afb2b14c5187df1923c82c19782c059037ab
Gonzalo Sian <gsian@baufest.com>
2025-04-14 09:33:11 -0300

    DAT-336 - Poder visualizar los negocios de los corredores
    Al ingresar el CUIT de un Corredor, traerá también los negocios en los cuales el cuit actúa como tal.

--- Molinos.DataAgro.Business/Managers/CupoManager.cs
+++ Molinos.DataAgro.Business/Managers/CupoManager.cs
@@ -6935,7 +6935,7 @@ namespace Molinos.DataAgro.Business.Managers
                     MaterialId = x.MaterialId,
                     FechaHasta = x.FechaHastaOriginal ?? x.FechaHasta,
                     KgPendientes = x.Cantidad
-                }, x => x.ProveedorId == proveedorId && x.MaterialId == materialId && x.Cantidad > 0 && x.TipoNegocioId != (int)EnumTipoNegocio.FIJACION &&
+                }, x => (x.ProveedorId == proveedorId || x.CorredorId == proveedorId) && x.MaterialId == materialId && x.Cantidad > 0 && x.TipoNegocioId != (int)EnumTipoNegocio.FIJACION &&
                     (sustentable ? x.Sustentable : epa || eudr ? x.EPA || x.EUDR : !x.Sustentable && !x.EPA && !x.EUDR) &&
                     (estadoId > 0 ? x.EstadoId == estadoId : x.EstadoId == (int)EnumEstadoContrato.Confirmado || x.EstadoId == (int)EnumEstadoContrato.Finalizado));
 
