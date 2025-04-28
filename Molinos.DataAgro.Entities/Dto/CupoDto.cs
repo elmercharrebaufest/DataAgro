2201330d73409f010db6c98fbc922b9adfbb48d2
Eugenio Martin <emartin@baufest.com>
2025-04-28 15:14:45 -0300

    DAT-365 - consulta cupos filtro por fecha registro

--- Molinos.DataAgro.Entities/Dto/CupoDto.cs
+++ Molinos.DataAgro.Entities/Dto/CupoDto.cs
@@ -26,7 +26,8 @@ namespace Molinos.DataAgro.Entities.Dto
         public bool? Fason { get; set; }
         public string Destinatario { get; set; }
         public DateTime FechaGeneracion { get; set; }
-        public DateTime? FechaRegistro => FechaGeneracion.Date;
+        //public DateTime? FechaRegistro => FechaGeneracion.Date;
+        public DateTime? FechaRegistro { get; set; }
         public int EstadoCupoId { get; set; }
         public string EstadoCupo { get; set; }
         public string MensajeError { get; set; }
