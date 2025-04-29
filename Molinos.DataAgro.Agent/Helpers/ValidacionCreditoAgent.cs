2db2758b31e6e526349a618807204552932ae241
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-04-29 19:49:36 +0000

    Merged PR 3359: DAT-397 Migración RFC Status contrato, Tipo de cambio, Validar credito
    DAT-397 Migración RFC Status contrato, Tipo de cambio, Validar credito, Validar Documento Proceso Pago

--- Molinos.DataAgro.Agent/Helpers/ValidacionCreditoAgent.cs
+++ Molinos.DataAgro.Agent/Helpers/ValidacionCreditoAgent.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac.Extras.NLog;
 using Molinos.DataAgro.Agent.ValidacionCredito;
+using Molinos.DataAgro.Agent.WS_GAQ_sin_PI;
 using Molinos.DataAgro.Entities.Dto;
 using Molinos.DataAgro.Entities.Entities;
 using Molinos.DataAgro.Entities.Helpers;
@@ -33,35 +34,69 @@ namespace Molinos.DataAgro.Agent
             {
                 try
                 {
-                    var agent = new SI_ZMPWS_DATAAGRO_VALIDAR_CREDITOClient();
-                    agent.ClientCredentials.UserName.UserName = UserSap;
-                    agent.ClientCredentials.UserName.Password = PassSap;
+                    if (ConfigurationManager.AppSettings["SAPsinPI"] == "1")
+                    {
+                        Z_MP_WS_DATAAGRO_DIRECTOClient agent = new Z_MP_WS_DATAAGRO_DIRECTOClient();
+                        agent.ClientCredentials.UserName.UserName = UserSap;
+                        agent.ClientCredentials.UserName.Password = PassSap;
 
-                    var rq = new Z_MPRFC_VALIDAR_CREDITO()
+                        var rq = new ZMprfcValidarCredito()
+                        {
+                            ImCuit = cuit,
+                        };
+                        var log = new Log
+                        {
+                            Fecha = DateTime.Now,
+                            Xml = rq.ToXml()
+                        };
+                        var logId = repositorio.Agregar(log);
+                        repositorio.GuardarCambios();
+                        logger.Debug(rq.ToXml());
+                        var valor = agent.ZMprfcValidarCredito(rq);
+                        logger.Debug(valor.ToXml());
+                        log = repositorio.Obtener<Log>(logId.Id);
+                        log.Xml += valor.ToXml();
+                        repositorio.GuardarCambios();
+                        var credito = new ValidarCreditoDto
+                        {
+                            Cuit = valor.ExCuit,
+                            Monto = valor.ExMonto,
+                            Moneda = valor.ExMoneda
+                        };
+                        return credito;
+                    }
+                    else
                     {
-                        IM_CUIT = cuit,
-                    };
-                    var log = new Log
-                    {
-                        Fecha = DateTime.Now,
-                        Xml = rq.ToXml()
-                    };
-                    var logId = repositorio.Agregar(log);
-                    repositorio.GuardarCambios();
-                    logger.Debug(rq.ToXml());
+                        var agent = new SI_ZMPWS_DATAAGRO_VALIDAR_CREDITOClient();
+                        agent.ClientCredentials.UserName.UserName = UserSap;
+                        agent.ClientCredentials.UserName.Password = PassSap;
 
-                    var valor = agent.SI_ZMPWS_DATAAGRO_VALIDAR_CREDITO(rq);
-                    logger.Debug(valor.ToXml());
-                    log = repositorio.Obtener<Log>(logId.Id);
-                    log.Xml += valor.ToXml();
-                    repositorio.GuardarCambios();
-                    var credito = new ValidarCreditoDto
-                    {
-                        Cuit = valor.EX_CUIT,
-                        Monto = valor.EX_MONTO,
-                        Moneda = valor.EX_MONEDA
-                    };
-                    return credito;
+                        var rq = new Z_MPRFC_VALIDAR_CREDITO()
+                        {
+                            IM_CUIT = cuit,
+                        };
+                        var log = new Log
+                        {
+                            Fecha = DateTime.Now,
+                            Xml = rq.ToXml()
+                        };
+                        var logId = repositorio.Agregar(log);
+                        repositorio.GuardarCambios();
+                        logger.Debug(rq.ToXml());
+
+                        var valor = agent.SI_ZMPWS_DATAAGRO_VALIDAR_CREDITO(rq);
+                        logger.Debug(valor.ToXml());
+                        log = repositorio.Obtener<Log>(logId.Id);
+                        log.Xml += valor.ToXml();
+                        repositorio.GuardarCambios();
+                        var credito = new ValidarCreditoDto
+                        {
+                            Cuit = valor.EX_CUIT,
+                            Monto = valor.EX_MONTO,
+                            Moneda = valor.EX_MONEDA
+                        };
+                        return credito;
+                    }
                 }
                 catch (Exception e)
                 {
