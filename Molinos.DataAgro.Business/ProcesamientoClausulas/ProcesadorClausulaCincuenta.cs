203dcf981accac5040ce6ad3612e88bd7899eccc
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-08-25 21:46:16 +0000

    Merged PR 3788: DAT-629 - Modificacion clausua de boletos y retirar clausula confirma
    Modificacion clausua de boletos y retirar clausula confirma

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuenta.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaCincuenta.cs
@@ -27,10 +27,16 @@ namespace Molinos.DataAgro.Business.Procesamiento
                 var esConvenio = clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && clausula.Basico.Madre == true;
                 if (esAFijarSinCanje || esConvenio || esFijacionDeContratoCanje)
                 {
-                    res.Texto += $"Las Partes acuerdan la posibilidad de prorrogar el plazo de pago indicado en las cláusulas previas, a opción del Vendedor. " +
-                    $"En caso de ejercer la opción al momento de fijar el precio, el Vendedor deberá notificar a Molinos Agro que deberá diferir el pago de la Mercadería por el plazo determinado, y el mismo no podrá extenderse de los 120 días. " +
-                    $"El Precio a pagar será neto de los impuestos y retenciones impositivas que correspondieran y se hubieran practicado según la condición del Vendedor y las particularidades del negocio. " +
-                    $"Toda vez que las Partes han acordado la opción de prorrogar la fecha de pago de la Mercadería, queda expresamente establecido el Vendedor no podrá invocar mora ni reclamar intereses y/o multas y/o cualquier tipo de penalidad por el tiempo transcurrido entre el plazo de pago originario y el del ejercicio de la opción de prórroga acordada en la presente Cláusula.";
+
+                    res.Texto += $"Las Partes acuerdan la posibilidad de prorrogar el plazo de pago indicado en las cláusulas previas. ";
+                    res.Texto += $"El Precio a pagar será neto de los impuestos y retenciones impositivas que correspondieran y se hubieran practicado según la condición del Vendedor y las particularidades del negocio. ";
+                    res.Texto += $"Toda vez que las Partes han acordado la posibilidad de prorrogar la fecha de pago de la Mercadería, queda expresamente establecido que el Vendedor no podrá invocar mora ni reclamar intereses y/o multas y/o cualquier tipo de penalidad por el tiempo transcurrido entre el plazo de pago originario y el del ejercicio de la opción de prórroga acordada en la presente Cláusula.";
+
+                    //res.Texto += $"Las Partes acuerdan la posibilidad de prorrogar el plazo de pago indicado en las cláusulas previas, a opción del Vendedor. " +
+                    //$"En caso de ejercer la opción al momento de fijar el precio, el Vendedor deberá notificar a Molinos Agro que deberá diferir el pago de la Mercadería por el plazo determinado, y el mismo no podrá extenderse de los 120 días. " +
+                    //$"El Precio a pagar será neto de los impuestos y retenciones impositivas que correspondieran y se hubieran practicado según la condición del Vendedor y las particularidades del negocio. " +
+                    //$"Toda vez que las Partes han acordado la opción de prorrogar la fecha de pago de la Mercadería, queda expresamente establecido el Vendedor no podrá invocar mora ni reclamar intereses y/o multas y/o cualquier tipo de penalidad por el tiempo transcurrido entre el plazo de pago originario y el del ejercicio de la opción de prórroga acordada en la presente Cláusula.";
+
                 }
             }
 
--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDieciocho.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDieciocho.cs
@@ -20,9 +20,9 @@ namespace Molinos.DataAgro.Business.Procesamiento
             var res = new ResultadoClausula();
             if (clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.CARTA_OFERTA)
             {
-                if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR)
+                if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && (clausula.Basico.CorredorId > 0 || clausula.Basico.ClasificacionId == (int)EnumClasificacionCompraNet.Acopiador))
                 {
-                    res.Texto = "En caso de que el Acopiador/Corredor no proceda a liquidar la mercadería dentro de las 72 horas corridas desde que la misma fuera entregada, aplicada y fijada, las Partes acuerdan que quedará a opción del Comprador determinar el día que se tomará válido para establecer el tipo de cambio a utilizar en los términos dispuestos en el presente boleto.";
+                    //res.Texto = "En caso de que el Acopiador/Corredor no proceda a liquidar la mercadería dentro de las 72 horas corridas desde que la misma fuera entregada, aplicada y fijada, las Partes acuerdan que quedará a opción del Comprador determinar el día que se tomará válido para establecer el tipo de cambio a utilizar en los términos dispuestos en el presente boleto.";
                 }
             }
             return res;
