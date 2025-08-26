35efc889f3a7d0a198fd6742d5b6989f55be6e78
Gonzalo Sian <gsian@baufest.com>
2025-08-26 09:58:34 -0300

    DAT-638 - Incorporar el criterio de Score Cupos al Algoritmo
    Se agrega el criterio de Score de Cupos al Algoritmo

---
+++ Molinos.DataAgro.Entities/Entities/CriterioScoreCupos.cs
@@ -0,0 +1,12 @@
+﻿using Molinos.DataAgro.Entities.CustomAtributte;
+using System.ComponentModel;
+
+namespace Molinos.DataAgro.Entities.Entities
+{
+    [Concreta(true)]
+    [DisplayName("Score Cupos")]
+    public class CriterioScoreCupos : Criterio
+    {
+        public override bool Concreta { get { return true; } }
+    }
+}
