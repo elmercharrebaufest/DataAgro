87c5bc9c03923e25ab65124f207579c7fa8aa844
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-09-04 17:10:21 +0000

    Merged PR 3831: DAT-688 Se modifica el criterio de las clausulas de bonificación
    DAT-688 Se modifica el criterio de las clausulas de bonificación

--- Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDiez.cs
+++ Molinos.DataAgro.Business/ProcesamientoClausulas/ProcesadorClausulaDiez.cs
@@ -20,7 +20,7 @@ namespace Molinos.DataAgro.Business.Procesamiento
             //SI EN EL CONTRATO SE ESPECIFICA UN PORCENTAJE DE MULTA DISTINTO DE CERO EN CONTRATOS A FIJAR
 
             var res = new ResultadoClausula();
-            if (clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.CARTA_OFERTA)
+            if (clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.CARTA_OFERTA && clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.CONFIRMA)
             {
                 if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && clausula.Basico.PlanCanje != true)
                 {
