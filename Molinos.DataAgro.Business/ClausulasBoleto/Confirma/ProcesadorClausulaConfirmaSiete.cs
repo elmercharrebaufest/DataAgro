83c6bae41451da24960a8ddd317cff6222f226d7
Evangelino Miranda <emiranda@baufest.com>
2025-09-16 16:33:10 -0500

    Modificacion clasificacion

--- Molinos.DataAgro.Business/ClausulasBoleto/Confirma/ProcesadorClausulaConfirmaSiete.cs
+++ Molinos.DataAgro.Business/ClausulasBoleto/Confirma/ProcesadorClausulaConfirmaSiete.cs
@@ -17,7 +17,7 @@ namespace Molinos.DataAgro.Business.ClausulasBoleto.Confirma
         {
             var res = new ResultadoClausula();
 
-            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && (clausula.Basico.ClasificacionContrato?.ToString().ToUpper() == "ACOPIADOR" || clausula.Basico.CorredorId > 0))
+            if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR && (clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "ACOPIADOR" || clausula.Basico.CorredorId > 0))
             {
                 res.Texto += "En caso que el Acopiador/Corredor no proceda a liquidar la mercadería dentro de las 72 horas corridas desde que la misma fuera " +
                     "entregada, aplicada y fijada, las Partes acuerdan que quedará a opción del Comprador determinar el día que se tomará válido para establecer " +
