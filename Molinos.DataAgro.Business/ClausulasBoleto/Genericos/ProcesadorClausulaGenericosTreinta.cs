0be1a5a3ea513db518208e6e311ecabc367d47af
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-09-23 20:29:16 +0000

    Merged PR 3915: DAT-780 Clausula Negocio a Fijar en dolares
    DAT-780 Clausula Negocio a Fijar en dolares

--- Molinos.DataAgro.Business/ClausulasBoleto/Genericos/ProcesadorClausulaGenericosTreinta.cs
+++ Molinos.DataAgro.Business/ClausulasBoleto/Genericos/ProcesadorClausulaGenericosTreinta.cs
@@ -22,14 +22,25 @@ namespace Molinos.DataAgro.Business.ClausulasBoleto.Genericos
             var res = new ResultadoClausula();
             if (clausula.Basico.BoletoId != (int)EnumBoletoCompraNet.FISICO)
             {
-                if (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO &&
-                    (clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "ACOPIADOR" ||
-                     clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "PRODUCTOR" ||
-                     clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "OTROS" ||
-                     clausula.Basico.CorredorId > 0) && clausula.Basico.Moneda == "USD")
+
+
+                if (
+                    (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO &&
+                     clausula.Basico.Moneda == "USD" &&
+                     clausula.Basico.CorredorId > 0 &&
+                        (clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "ACOPIADOR" ||
+                         clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "PRODUCTOR" ||
+                         clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "OTROS")
+                        ) ||
+                    (clausula.Basico.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO &&
+                     clausula.Basico.Moneda == "USD" &&
+                        (clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "ACOPIADOR" ||
+                        clausula.Basico.ClasificacionDescripcion?.ToString().ToUpper() == "OTROS")
+                    )
+                   )
                 {
                     res.Texto += "Toda vez que el Acopiador/Corredor no proceda a liquidar la mercadería dentro de las 72 horas desde que la misma fuera entregada y aplicada, " +
-                        "las Partes acuerdan que quedará a opción del Comprador determinar el día que se tomará válido para establecer el tipo de cambio a utilizar en los términos dispuestos en el presente boleto.";
+                                 "las Partes acuerdan que quedará a opción del Comprador determinar el día que se tomará válido para establecer el tipo de cambio a utilizar en los términos dispuestos en el presente boleto.";
                 }
             }
             return res;
