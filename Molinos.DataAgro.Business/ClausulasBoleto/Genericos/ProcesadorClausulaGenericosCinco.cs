a2942194c3070fd54a28eaa6ed2de12bc0ea7cb1
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2025-10-09 17:05:31 +0000

    Updated ProcesadorClausulaGenericosCinco.cs

--- Molinos.DataAgro.Business/ClausulasBoleto/Genericos/ProcesadorClausulaGenericosCinco.cs
+++ Molinos.DataAgro.Business/ClausulasBoleto/Genericos/ProcesadorClausulaGenericosCinco.cs
@@ -38,7 +38,7 @@ namespace Molinos.DataAgro.Business.ClausulasBoleto.Genericos
                         decimal porcentajelFueraPrecio = (decimal)(descuentoGeneralFueraPrecio?.Porcentaje < 0 ? descuentoGeneralFueraPrecio?.Porcentaje * -1 : descuentoGeneralFueraPrecio?.Porcentaje);
                         decimal importeFueraPrecio = (decimal)(descuentoGeneralFueraPrecio?.Importe < 0 ? descuentoGeneralFueraPrecio?.Importe * -1 : descuentoGeneralFueraPrecio?.Importe);
                         decimal descuentoImporteFueraPrecio = (importeFueraPrecio * porcentajelFueraPrecio) / 100;
-                        importeFueraPrecio = porcentajelFueraPrecio > 0 ? Math.Round((importeFueraPrecio - descuentoImporteFueraPrecio), 2) : importeFueraPrecio;
+                        importeFueraPrecio = descuentoGeneralFueraPrecio?.Porcentaje > 0 ? Math.Round((importeFueraPrecio - descuentoImporteFueraPrecio), 2) : Math.Round((importeFueraPrecio + descuentoImporteFueraPrecio), 2);
 
                         if (descuentoGeneralFueraPrecio?.Porcentaje > 0)
                         {
@@ -72,7 +72,7 @@ namespace Molinos.DataAgro.Business.ClausulasBoleto.Genericos
                         decimal porcentajelSobrePrecio = (decimal)(descuentoGeneralSobrePrecio?.Porcentaje < 0 ? descuentoGeneralSobrePrecio?.Porcentaje * -1 : descuentoGeneralSobrePrecio?.Porcentaje);
                         decimal importeSobrePrecio = (decimal)(descuentoGeneralSobrePrecio?.Importe < 0 ? descuentoGeneralSobrePrecio?.Importe * -1 : descuentoGeneralSobrePrecio?.Importe);
                         decimal descuentoImporteFueraPrecio = (importeSobrePrecio * porcentajelSobrePrecio) / 100;
-                        importeSobrePrecio = porcentajelSobrePrecio > 0 ? Math.Round((importeSobrePrecio - descuentoImporteFueraPrecio), 2) : importeSobrePrecio;
+                        importeSobrePrecio = descuentoGeneralSobrePrecio?.Porcentaje > 0 ? Math.Round((importeSobrePrecio - descuentoImporteFueraPrecio), 2) : Math.Round((importeSobrePrecio + descuentoImporteFueraPrecio), 2);
 
                         // En la bonificacion sobre el precio siempre debe mostrarse lo que se tiene como apertura de precio
                         if (descuentoGeneralSobrePrecio?.Porcentaje > 0)
