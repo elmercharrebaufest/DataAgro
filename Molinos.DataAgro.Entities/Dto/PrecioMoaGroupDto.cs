8733c3429afbcd9ff9011c1754a923e43bcd47d2
Gonzalo Sian <gsian@baufest.com>
2025-11-26 17:00:43 -0300

    DAT-873 - Migrar servicios REST a SOAP para MOA Operaciones

---
+++ Molinos.DataAgro.Entities/Dto/PrecioMoaGroupDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Molinos.DataAgro.Entities.Dto
+{
+    public class PrecioMoaGroupDto
+    {
+        public int MaterialId { get; set; }
+        public List<PrecioMoaCompraNetDto> PrecioMoaCompraNetDtoList { get; set; }
+    }
+}
