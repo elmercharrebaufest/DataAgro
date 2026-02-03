b780113a273be35065e5b92fadf285719233e23b
facundobrandi <facundobrandi20@hotmail.com>
2026-02-03 15:16:28 -0300

    Dev - Correcion de postDeployment

--- Base de Datos/dbo/Script Posdeployment/Script.PostDeployment1.sql
+++ Base de Datos/dbo/Script Posdeployment/Script.PostDeployment1.sql
@@ -352,10 +352,10 @@ IF NOT EXISTS (select 1 from CierreCupera where MaterialId = (select top 1 Mater
 IF NOT EXISTS (select 1 from CierreCupera where MaterialId = (select top 1 MaterialId from Material where Descripcion ='Girasol AO')) BEGIN insert into CierreCupera (MaterialId, Cierre) values ((select top 1 MaterialId from Material where Descripcion ='Girasol AO'), 0); END
 
 IF NOT EXISTS (select 1 from Rol where Descripcion = 'Recibir Sugerencia FAQ') BEGIN insert into Rol (Descripcion) values ('Recibir Sugerencia FAQ'); END
-IF NOT EXISTS (select 1 from RolPermiso where RolId = (select RolId from Rol where Descripcion ='Recibir Sugerencia FAQ') and Permiso = 50) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Recibir Sugerencia FAQ'), 50); END
-IF NOT EXISTS (select 1 from RolPermiso where RolId = (select RolId from Rol where Descripcion ='Algoritmo de Cupos') and Permiso = 735) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Algoritmo de Cupos'), 735); END
-IF NOT EXISTS (select 1 from RolPermiso where RolId = (select RolId from Rol where Descripcion ='Administrador') and Permiso = -1) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Administrador'), -1); END
-IF NOT EXISTS (select 1 from RolPermiso where RolId = (select RolId from Rol where Descripcion ='BoletoAdmin') and Permiso = 920) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='BoletoAdmin'), 920); END
+IF NOT EXISTS (select 1 from RolPermiso where RolId = (select Id from Rol where Descripcion ='Recibir Sugerencia FAQ') and Permiso = 50) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Recibir Sugerencia FAQ'), 50); END
+IF NOT EXISTS (select 1 from RolPermiso where RolId = (select Id from Rol where Descripcion ='Algoritmo de Cupos') and Permiso = 735) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Algoritmo de Cupos'), 735); END
+IF NOT EXISTS (select 1 from RolPermiso where RolId = (select Id from Rol where Descripcion ='Administrador') and Permiso = -1) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='Administrador'), -1); END
+IF NOT EXISTS (select 1 from RolPermiso where RolId = (select Id from Rol where Descripcion ='BoletoAdmin') and Permiso = 920) BEGIN insert into RolPermiso (RolId, Permiso) values ((select Id from Rol where Descripcion ='BoletoAdmin'), 920); END
 
 --ResearchCoeficienteCultivo
 IF NOT EXISTS (select 1 from ResearchCoeficienteCultivo where MaterialId = (select MaterialId from Material where Descripcion='Trigo') and Coeficiente='0.9') BEGIN insert into ResearchCoeficienteCultivo (MaterialId, Coeficiente) values((select MaterialId from Material where Descripcion='Trigo'),'0.9'); END
