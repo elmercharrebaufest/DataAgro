e380afe63e55750c23261c639a81524f755ba5e8
Gonzalo Sian <gsian@baufest.com>
2026-02-05 08:59:58 -0300

    revert: rollback changes introduced by DAT-645
    This commit reverts the changes related to ticket DAT-645, restoring the previous behavior.

--- Molinos.DataAgro.Entities/Dto/DatosFijacionDeContratoDto.cs
+++ Molinos.DataAgro.Entities/Dto/DatosFijacionDeContratoDto.cs
@@ -56,7 +56,7 @@ namespace Molinos.DataAgro.Entities.Dto
         public decimal? ImporteSustentable { get; set; }
         public Moneda MonedaSustentable { get; set; }
         public int? SustentableTipoDBId { get; set; }
-        public int? KgMaximos { get; set; }
-        public int? KgMinimos { get; set; }
+        //public int? KgMaximos { get; set; }
+        //public int? KgMinimos { get; set; }
     }
 }
