308c357e8522f67745182a8127eab604c3b91abc
Evangelino Miranda Tecse <emiranda@baufest.com>
2026-04-07 12:27:42 -0500

    DAT-1062

--- Molinos.DataAgro.Business/Managers/BoletoManager.cs
+++ Molinos.DataAgro.Business/Managers/BoletoManager.cs
@@ -1123,12 +1123,13 @@ namespace Molinos.DataAgro.Business.Managers
             var mensaje = boleto.Estado_Version;
             try
             {
-                if (boleto.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR || boleto.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO)
-                    boleto.FijacionSAP = "1";
-                else
-                    boleto.FijacionSAP = "2";
+                //if (boleto.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR || boleto.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO)
+                //    boleto.FijacionSAP = "1";
+                //else
+                //    boleto.FijacionSAP = "2";
 
-                var consultaBoleto = oConsultarEstadoBoletoAgent.EstadoBoleto(boleto.ContratoSAP, boleto.FijacionSAP ?? string.Empty);
+                boleto.FijacionSAP = " ";
+                var consultaBoleto = oConsultarEstadoBoletoAgent.EstadoBoleto(boleto.ContratoSAP, boleto.FijacionSAP);
                 var version = Int32.Parse(consultaBoleto.Version);
                 if (version > boleto.Version)
                 {
--- Molinos.DataAgro.Business/Managers/ConfirmaManager.cs
+++ Molinos.DataAgro.Business/Managers/ConfirmaManager.cs
@@ -1419,12 +1419,12 @@ namespace Molinos.DataAgro.Business.Managers
             var mensaje = boleto.Estado_Version;
             try
             {
-                if (boleto.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR || boleto.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO)
-                    boleto.FijacionSAP = "1";
-                else
-                    boleto.FijacionSAP = "2";
-                
-                var consultaBoleto = oConsultarEstadoBoletoAgent.EstadoBoleto(boleto.ContratoSAP, boleto.FijacionSAP ?? string.Empty);
+                //if (boleto.TipoNegocioId == (int)EnumTipoNegocio.A_FIJAR || boleto.TipoNegocioId == (int)EnumTipoNegocio.A_PRECIO)
+                //    boleto.FijacionSAP = "1";
+                //else
+                //    boleto.FijacionSAP = "2";
+                boleto.FijacionSAP = " ";
+                var consultaBoleto = oConsultarEstadoBoletoAgent.EstadoBoleto(boleto.ContratoSAP, boleto.FijacionSAP);
                 var version = Int32.Parse(consultaBoleto.Version);
                 if (version > boleto.Version)
                 {
