332eb8af33dc3542d92d0ce4f141564717c38e68
EXT Miranda tecse, Evangelino <Evangelino.Mirandatecse@molinosagro.com.ar>
2026-05-20 14:10:35 +0000

    Merged PR 5260: DAT-1008

--- Molinos.DataAgro.Agent/Helpers/ModificacionContratoControlBoletoAgent.cs
+++ Molinos.DataAgro.Agent/Helpers/ModificacionContratoControlBoletoAgent.cs
@@ -1,4 +1,4 @@
-﻿using Molinos.DataAgro.Agent.WS_GAQ_sin_PI;
+using Molinos.DataAgro.Agent.WS_GAQ_sin_PI;
 using Molinos.DataAgro.Entities.Dto.ControlDeBoletos;
 using Molinos.DataAgro.Entities.Helpers;
 using Molinos.DataAgro.Interfaces.Agent;
@@ -18,7 +18,6 @@ namespace Molinos.DataAgro.Agent.Helpers
         private readonly string passSap = ConfigurationManager.AppSettings["SapPass"];
         private readonly bool activarLogDebug = ConfigurationManager.AppSettings["ActivarLogDebug"] == "1";
         private readonly bool valorPruebaSap = ConfigurationManager.AppSettings["ValorPruebaSap"] == "1";
-        private readonly bool sapSinPi = ConfigurationManager.AppSettings["SAPsinPI"] == "1";
 
         public ModificacionContratoControlBoletoAgent(ILogger logger)
         {
@@ -31,14 +30,6 @@ namespace Molinos.DataAgro.Agent.Helpers
             if (valorPruebaSap)
             {
                 return "Modificación de contrato satisfactoria";
-            }
-
-            // SAP con PI no implementado
-            if (!sapSinPi)
-            {
-                throw new NotImplementedException(
-                    "El servicio SAP con PI no está implementado en esta versión."
-                );
             }
 
             try
@@ -56,15 +47,12 @@ namespace Molinos.DataAgro.Agent.Helpers
                     ImProvincia = controlDeBoletosModificarContrato.Provincia,
                     ImUsuario = controlDeBoletosModificarContrato.Usuario
                 };
-
+                logger.Debug(request.ToXml());
                 var response = agent.ZMprfcModContCtrBoleto(request);
-
                 if (activarLogDebug)
                 {
-                    logger.Debug(request.ToXml());
                     logger.Debug(response.ToXml());
                 }
-
                 return response.ExMensaje?.Trim();
             }
             catch (Exception ex)
