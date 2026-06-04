2d12d11c640bbe2fa6d54551bf1174f5d0a7354f
facundobrandi <facundobrandi20@hotmail.com>
2026-06-04 15:57:25 -0300

    Correcion sobre Formato de Excel

--- Molinos.DataAgro.Business/Managers/CupoManager.cs
+++ Molinos.DataAgro.Business/Managers/CupoManager.cs
@@ -1657,7 +1657,7 @@ namespace Molinos.DataAgro.Business.Managers
                   .Select(row => new AltaMasivaCupoDto
                   {
                       ContratoSAP = row["ContratoSAP"]?.ToString().PadLeft(10, '0'),
-                      FechaSugerida = DateTime.FromOADate(double.Parse(row["FechaSugerida"]?.ToString())),
+                      FechaSugerida = row.Field<DateTime>("FechaSugerida"),
                       CantidadDeCupos = int.Parse(row["CantidadDeCupos"]?.ToString()),
                   })
                   .Where(x => !string.IsNullOrEmpty(x.ContratoSAP))
