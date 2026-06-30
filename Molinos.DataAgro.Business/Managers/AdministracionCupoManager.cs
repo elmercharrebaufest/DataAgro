04c1ae56104d9ac0fe5cb19e42d52bd1b4bcd30e
facundobrandi <facundobrandi20@hotmail.com>
2026-06-30 09:53:54 -0300

    DAT-1145 Correciones para tomar en cuenta posibles nulos

--- Molinos.DataAgro.Business/Managers/AdministracionCupoManager.cs
+++ Molinos.DataAgro.Business/Managers/AdministracionCupoManager.cs
@@ -107,7 +107,8 @@ namespace Molinos.DataAgro.Business.Managers
                         FechaIngreso = solicitud.Fecha,
                         ZonaCupoId = solicitud.ZonaId,
                         ComercialId = solicitud.ComercialId,
-                        Calidad = solicitud.SugerenciaCupo.StandardDeCalidad == "Cámara" || solicitud.SugerenciaCupo.StandardDeCalidad == "Camara" ? "Camara" : "Fabrica",
+                        Calidad = solicitud.SugerenciaCupo.StandardDeCalidad == null ? null : solicitud.SugerenciaCupo.StandardDeCalidad == "Cámara"
+                        || solicitud.SugerenciaCupo.StandardDeCalidad == "Camara" ? "Camara" : "Fabrica",
                         Fason = false,
                         Destinatario = solicitud.SugerenciaCupo.Destinatario,
                         FechaGeneracion = DateTime.Now,
