dc931339caec7d37d8e7b3e6912dc49bc82ab729
Gonzalo Sian <gsian@baufest.com>
2026-08-06 09:46:38 -0300

    DAT-1240 - Crear nuevo contrato IDataAgroServicesFull para el servicio
    Se crea una nueva interfaz para los métodos usados por MOA Operaciones y Scato, para que el actual sea exclusivo de SAP

--- WebDataAgro/App_Start/Startup.Dependencias.cs
+++ WebDataAgro/App_Start/Startup.Dependencias.cs
@@ -15,7 +15,11 @@ namespace WebDataAgro.App_Start
         {
             builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
 
-            builder.RegisterType<DataAgroServices>().As<IDataAgroServices>().InstancePerLifetimeScope();
+            builder.RegisterType<DataAgroServices>()
+                .As<IDataAgroServices>()
+                .As<IDataAgroServicesFull>()
+                .InstancePerLifetimeScope();
+
             builder.RegisterType<AuthService>().As<IAuthService>().InstancePerLifetimeScope();
             builder.RegisterType<DataAgroDbContext>().As<DbContext>().InstancePerLifetimeScope();
             builder.RegisterType<RepositorioEF>().As<IRepositorio>().InstancePerLifetimeScope();
